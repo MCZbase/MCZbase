MCZbase/MCZbase

commit 61e4e842ea4a4d895f43b25e10bcc2dc5e269c8c

ISSUE:  PURPOSE: Changing search for observations.  DESCRIPTION: Change to query to properly select/deselect observations.

includes/SearchSql.cfm
@@ -291,8 +291,13 @@ true) OR (isdefined("collection_id") AND collection_id EQ 13)>
 	<cfset mapurl = "#mapurl#&encumbering_agent_id=#encumbering_agent_id#">
 </cfif>
 <cfif isdefined("collection_id") AND len(collection_id) gt 0>
+	<cfif (isdefined("session.ShowObservations") AND session.ShowObservations is true)>
+        <cfset basQual = "#basQual#  AND (#session.flatTableName#.collection_id IN ( #collection_id#, 13 ) or #session.flatTableName#.associated_collection IN ( #collection_id#, 13 ))" >
+        <cfset mapurl = "#mapurl#&collection_id=#collection_id#">
+	<cfelse>
         <cfset basQual = "#basQual#  AND (#session.flatTableName#.collection_id IN ( #collection_id# ) or #session.flatTableName#.associated_collection IN ( #collection_id# ))" >
         <cfset mapurl = "#mapurl#&collection_id=#collection_id#">
+</cfif>
 </cfif>
 <cfif isdefined("session.collection") and len(session.collection) gt 0>
 	<cfset collection_cde=session.collection>

commit 66b003867869e2a6b254d159fb6bf2c17f9458b3

ISSUE: BugID: 1579 PURPOSE: Fix to error introduced in 61e4e842. DESCRIPTION: Removing extra </cfif> tag.

includes/SearchSql.cfm
@@ -299,7 +299,6 @@ true) OR (isdefined("collection_id") AND collection_id EQ 13)>
         <cfset mapurl = "#mapurl#&collection_id=#collection_id#">
 	</cfif>
 </cfif>
-</cfif>
 <cfif isdefined("session.collection") and len(session.collection) gt 0>
 	<cfset collection_cde=session.collection>
 </cfif>