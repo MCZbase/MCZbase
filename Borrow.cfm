MCZbase/MCZbase

commit 35283f4fd1f455cd17b8f04f97d20b389e9183e0

ISSUE: Redmine 285 PURPOSE: Lender Loan Type not saved from new Borrow. DESCRIPTION: Adding to save new section, putting some more save new fields into cfqueryparam tags.

Borrow.cfm
@@ -1386,7 +1386,7 @@ $(function() {
 			NATURE_OF_MATERIAL,
 			COLLECTION_ID)
 		VALUES (
-			#transaction_id#,
+			<cfqueryparam cfsqltype="CF_SQL_DECIMAL" value="#transaction_id#">,
 			'#dateformat(TRANS_DATE,"yyyy-mm-dd")#',
 			'#escapeQuotes(TRANS_REMARKS)#',
 			'borrow',
@@ -1403,22 +1403,24 @@ $(function() {
 			RECEIVED_DATE,
 			DUE_DATE,
 			LENDERS_LOAN_DATE,
+			LENDERS_LOAN_TYPE,
 			LENDERS_INSTRUCTIONS,
             DESCRIPTION_OF_BORROW,
             no_of_specimens,
 			BORROW_STATUS
 		) VALUES (
-			#transaction_id#,
+			<cfqueryparam cfsqltype="CF_SQL_DECIMAL" value="#transaction_id#">,
 			'#LENDERS_TRANS_NUM_CDE#',
 			'#Borrow_Number#',
 			#LENDERS_INVOICE_RETURNED_FG#,
 			'#dateformat(RECEIVED_DATE,"yyyy-mm-dd")#',
 			'#dateformat(DUE_DATE,"yyyy-mm-dd")#',
 			'#dateformat(LENDERS_LOAN_DATE,"yyyy-mm-dd")#',
-            '#escapeQuotes(LENDERS_INSTRUCTIONS)#',
-            '#DESCRIPTION_OF_BORROW#',
-            '#no_of_specimens#',
-			'#BORROW_STATUS#'
+            		<cfqueryparam cfsqltype="CF_SQL_VARCHAR" value="#lenders_loan_type#">,
+            		<cfqueryparam cfsqltype="CF_SQL_VARCHAR" value="#LENDERS_INSTRUCTIONS#">,
+            		<cfqueryparam cfsqltype="CF_SQL_VARCHAR" value="#DESCRIPTION_OF_BORROW#">,
+            		<cfqueryparam cfsqltype="CF_SQL_VARCHAR" value="#no_of_specimens#">,
+            		<cfqueryparam cfsqltype="CF_SQL_VARCHAR" value="#BORROW_STATUS#">
 		)
 		</cfquery>
 		<cfquery name="authBy" datasource="user_login" username="#session.dbuser#" password="#decrypt(session.epw,cfid)#">
@@ -1448,8 +1450,8 @@ $(function() {
 			    agent_id,
 			    trans_agent_role
 			) values (
-				#transaction_id#,
-				#received_from_agent_id#,
+				<cfqueryparam cfsqltype="CF_SQL_DECIMAL" value="#transaction_id#">,
+				<cfqueryparam cfsqltype="CF_SQL_DECIMAL" value="#received_from_agent_id#">,
 				'received from'
 			)
 		</cfquery>

commit 91f0861429231a107a534712bd1840000e72b809

ISSUE: Redmine 285 PURPOSE: Bugfix to adding/saving new borrows introduced in 35283f4f. DESCRIPTION: Field naming inconsistencies led to errors, field is lender_loan_type, not lenders_loan_type.  Fixed.

Borrow.cfm
@@ -1170,8 +1170,7 @@ $(function() {
 			<tr>
 				<td>
 					<label for="lender_loan_type">Lender's Loan Type</label>
-					<input type="text" name="lender_loan_type" id="lender_loan_type"
-						value="#getBorrow.lender_loan_type#">
+					<input type="text" name="lender_loan_type" id="lender_loan_type" >
 				</td>
 				<td>
 					<label for="lenders_loan_date">Lender's Loan Date</label>
@@ -1403,7 +1402,7 @@ $(function() {
 			RECEIVED_DATE,
 			DUE_DATE,
 			LENDERS_LOAN_DATE,
-			LENDERS_LOAN_TYPE,
+			lender_loan_type,
 			LENDERS_INSTRUCTIONS,
             DESCRIPTION_OF_BORROW,
             no_of_specimens,
@@ -1416,7 +1415,7 @@ $(function() {
 			'#dateformat(RECEIVED_DATE,"yyyy-mm-dd")#',
 			'#dateformat(DUE_DATE,"yyyy-mm-dd")#',
 			'#dateformat(LENDERS_LOAN_DATE,"yyyy-mm-dd")#',
-            		<cfqueryparam cfsqltype="CF_SQL_VARCHAR" value="#lenders_loan_type#">,
+            		<cfqueryparam cfsqltype="CF_SQL_VARCHAR" value="#lender_loan_type#">,
             		<cfqueryparam cfsqltype="CF_SQL_VARCHAR" value="#LENDERS_INSTRUCTIONS#">,
             		<cfqueryparam cfsqltype="CF_SQL_VARCHAR" value="#DESCRIPTION_OF_BORROW#">,
             		<cfqueryparam cfsqltype="CF_SQL_VARCHAR" value="#no_of_specimens#">,