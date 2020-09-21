e50943455c08f1480c0a3611ee1fca6a8269fe6c
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2020-09-21 16:06:35 -0400

    ISSUE: 542  PURPOSE:  Dialog boxes  DESC:  Style dialogs -- 300 px width not working on larger sizes, why?

--- transactions/js/transactions.js
+++ transactions/js/transactions.js
@@ -1081,7 +1081,7 @@ function openTransProjectLinkDialog(transaction_id, dialogId, projectsDivId) {
 		dialogClass: 'dialog_fixed,ui-widget-header',
 		modal: true,
 		stack: true,
-		minWidth: 300,
+		minWidth: 450,
 		minHeight: 200,
 		draggable:true,
 		buttons: {
