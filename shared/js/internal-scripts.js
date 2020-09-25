ac2ad2e0b8379c7b89deba9052e1560b52fa775d
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2020-09-25 14:22:01 -0400

    ISSUE: NotForMeeting #709 (InProgress): Redesign all Transaction pages, put on production [parent]  DESC:  styling media pages.

--- shared/js/internal-scripts.js
+++ shared/js/internal-scripts.js
@@ -267,10 +267,6 @@ function addRelationTo (n,targetId) {
 	nSel.value='';
 	nDiv.appendChild(nSel);
 
-	c = document.createElement("textNode");
-	c.innerHTML="Hi There";
-	nDiv.appendChild(c);
-
 	var n1=n-1;
 	var inpName='related_value__' + n1;
 	var nInp = document.getElementById(inpName).cloneNode(true);
