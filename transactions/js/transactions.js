031b614d4b7d1a6751dd32bb2c7674accb1d5541
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2020-10-27 12:39:13 -0400

    Test loader -- removed it.

--- transactions/js/transactions.js
+++ transactions/js/transactions.js
@@ -438,12 +438,6 @@ function loadShipments(transaction_id) {
 			method : "getShipmentsByTransHtml",
 			transaction_id : transaction_id
 		},
-		beforeSend: function(result){
-			$('#loading-spinner').show();
-		},
-		complete: function(result) {
-			$('#loading-spinner').hide();
-		},
 		success: function (result) {
 			$("#shipmentTable").html(result);
 		},
