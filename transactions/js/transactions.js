6e7524d79f963a9dd278033a7087403c3d8ef6e9
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2020-11-19 17:08:18 -0500

    ISSUE:  669  PURPOSE: Universal Styles  DESC: agent table

--- transactions/js/transactions.js
+++ transactions/js/transactions.js
@@ -721,7 +721,7 @@ function addTransAgentToForm (id,name,role,formid) {
 		},
 		function (data) {
 			var i=parseInt($('#numAgents').val())+1;
-			var d=  '<section class="alert alert-warning px-0 pt-1 pb-2 my-1 border-top border-bottom">'+ i +'<div class="input-group"><div class="col-12 col-md-5 mb-0"><div class="input-group">';
+			var d=  '<section class="alert alert-warning my-1 py-1 border-top border-bottom">'+ i +'<div class="col-12 col-md-4"><div class="input-group">';
 			d+='<input type="hidden" id="agent_id_' + i + '" name="agent_id_' + i + '" value="' + id + '" ';
 			d+=' onchange=" updateAgentLink($(\'#agent_id_' + i +'\').val(),\'agentViewLink_' + i + '\'); " >';
 			d+='<span id="agentViewLink_' + i + '" class="px-2"></span>';
@@ -729,7 +729,7 @@ function addTransAgentToForm (id,name,role,formid) {
 			d+='<div class="input-group"><div class="input-group-prepend">';
 			d+='<span class="input-group-text smaller" id="agent_icon_'+i+'"><i class="fa fa-user" aria-hidden="true"></i></span> </div>';
 			d+='<input type="text" id="trans_agent_' + i + '" name="trans_agent_' + i + '" required class="goodPick form-control form-control-sm data-entry-input" size="30" value="' + name + '" >';
-			d+='</div></div></div><div class="col-10 col-md-3 mb-0">';
+			d+='</div></div></div><div class="col-10 col-md-4 mb-0">';
 			d+='<select name="trans_agent_role_' + i + '" id="trans_agent_role_' + i + '" class="data-entry-select">';
 			for (a=0; a<data.ROWCOUNT; ++a) {
 				d+='<option ';
@@ -739,7 +739,7 @@ function addTransAgentToForm (id,name,role,formid) {
 				d+=' value="' + data.DATA.TRANS_AGENT_ROLE[a] + '">'+ data.DATA.TRANS_AGENT_ROLE[a] +'</option>';
 			}
 			d+='</select></div><div class="col-12 col-md-3">';
-			d+='<div class="input-group"><div class="input-group-prepend"><div class="input-group-text bg-transparent border-0"><input class="position-relative" type="checkbox" name="del_agnt_' + i + '" name="del_agnt_' + i + '" value="1">Delete?';
+			d+='<div class="input-group"><div class="input-group-prepend"><div class="input-group-text bg-transparent border-0"><input class="position-relative" type="checkbox" name="del_agnt_' + i + '" name="del_agnt_' + i + '" value="1"> Delete?';
 			d+='</div></div></div></div>';
 			d+='</section>';
 		
