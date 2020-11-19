95c0ff542c6e5f76b698b9bcf1a08ff626bae89b
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2020-11-19 14:43:14 -0500

    ISSUE:  669  PURPOSE: Universal Styles  DESC: Removing clone from rows

--- transactions/js/transactions.js
+++ transactions/js/transactions.js
@@ -741,7 +741,6 @@ function addTransAgentToForm (id,name,role,formid) {
 			d+='</select></div><div class="col-2 col-md-1 px-0 mb-0"><label class="form-check-label data-entry-label pl-0 smaller">Delete?</label>';
 			d+='<div class="form-check"><input type="checkbox" name="del_agnt_' + i + '" name="del_agnt_' + i + '" value="1" class="checkbox-inline">';
 			d+='</div></div><div class="col-10 col-md-3 mb-0"><label class="data-entry-label">Clone As</label>';
-	
 			d+='</div></section>';
 			d+='<script>';
 			d+=' $(document).ready(function() {';
