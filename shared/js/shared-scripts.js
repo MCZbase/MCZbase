56cb0972f60f5206be40317093fd16df427aa3d4
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-02-16 13:54:33 -0500

    Issue 754  Keyboard and accessible navigation for tabs on search pages (transactions, specimens)

--- shared/js/shared-scripts.js
+++ shared/js/shared-scripts.js
@@ -921,7 +921,7 @@ function changeTabs(e) {
   // Remove all current selected tabs
   parent
     .querySelectorAll('[aria-selected="true"]')
-    .forEach(t => t.setAttribute("aria-hidden", true));
+    .forEach(t => t.setAttribute("aria-selected", false));
 
   // Set this tab as selected
   target.setAttribute("aria-selected", true);
@@ -929,12 +929,12 @@ function changeTabs(e) {
   // Hide all tab panels
   grandparent
     .querySelectorAll('[role="tabpanel"]')
-    .forEach(p => p.setAttribute("aria-hidden", true));
+    .forEach(p => p.setAttribute("hidden", true));
 
   // Show the selected panel
   grandparent.parentNode
     .querySelector(`#${target.getAttribute("aria-controls")}`)
-    .removeAttribute("aria-hidden");
+    .removeAttribute("hidden");
 }
  
 
