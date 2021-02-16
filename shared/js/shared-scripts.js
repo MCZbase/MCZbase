dc6027cbcd569b3b3438298a270083612398a767
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-02-16 11:54:05 -0500

    Issue 754  Keyboard and accessible navigation for tabs on search pages (transactions, specimens)

--- shared/js/shared-scripts.js
+++ shared/js/shared-scripts.js
@@ -882,7 +882,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // Add a click event handler to each tab
   tabs.forEach(tab => {
-    tab.addEventListener("click", changeTabs);
+    tab.addEventListener("click",getElementsByClassName("show"));
   });
 
   // Enable arrow navigation between tabs in the tab list
