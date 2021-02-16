f1d9ceeaaa57126066819c5eafe8465a74249afa
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-02-16 11:40:28 -0500

    Issue 754  Keyboard and accessible navigation for tabs on search pages (transactions, specimens)

--- shared/js/shared-scripts.js
+++ shared/js/shared-scripts.js
@@ -913,28 +913,28 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-function changeTabs(e) {
-  const target = e.target;
-  const parent = target.parentNode;
-  const grandparent = parent.parentNode;
-
-  // Remove all current selected tabs
-  parent
-    .querySelectorAll('[aria-selected="true"]')
-    .forEach(t => t.setAttribute("aria-selected", false));
-
-  // Set this tab as selected
-  target.setAttribute("aria-selected", true);
-
-  // Hide all tab panels
-  grandparent
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach(p => p.setAttribute("hidden", true));
-
-  // Show the selected panel
-  grandparent.parentNode
-    .querySelector(`#${target.getAttribute("aria-controls")}`)
-    .removeAttribute("hidden");
-}
+//function changeTabs(e) {
+//  const target = e.target;
+//  const parent = target.parentNode;
+//  const grandparent = parent.parentNode;
+//
+//  // Remove all current selected tabs
+//  parent
+//    .querySelectorAll('[aria-selected="true"]')
+//    .forEach(t => t.setAttribute("aria-selected", false));
+//
+//  // Set this tab as selected
+//  target.setAttribute("aria-selected", true);
+//
+//  // Hide all tab panels
+//  grandparent
+//    .querySelectorAll('[role="tabpanel"]')
+//    .forEach(p => p.setAttribute("hidden", true));
+//
+//  // Show the selected panel
+//  grandparent.parentNode
+//    .querySelector(`#${target.getAttribute("aria-controls")}`)
+//    .removeAttribute("hidden");
+//}
  
 
