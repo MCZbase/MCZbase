365d5082e15c04ff2da3f9628a42dc240416a66f
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-02-17 10:25:04 -0500

    Issue 754  Keyboard and accessible navigation for tabs on search pages (transactions, specimens)

--- shared/js/shared-scripts.js
+++ shared/js/shared-scripts.js
@@ -888,30 +888,47 @@ window.addEventListener("DOMContentLoaded", () => {
   // Enable arrow navigation between tabs in the tab list
   let tabFocus = 0;
 
-  document.addEventListener("keydown", function(e) {
-    // Move right
-    if (e.keyCode === 39 || e.keyCode === 37) {
-      tabs[tabFocus].setAttribute("tabindex", -1);
-      if (e.keyCode === 39) {
-        tabFocus++;
-        // If we're at the end, go to the start
-        if (tabFocus >= tabs.length) {
-          tabFocus = 0;
-        }
-        // Move left
-      } else if (e.keyCode === 37) {
-        tabFocus--;
-        // If we're at the start, move to the end
-        if (tabFocus < 0) {
-          tabFocus = tabs.length - 1;
-        }
-      }
+ keydown: function(ev){
+      var LEFT_ARROW = 37;
+      var UP_ARROW = 38;
+      var RIGHT_ARROW = 39;
+      var DOWN_ARROW = 40;
 
-      tabs[tabFocus].setAttribute("tabindex", 0);
-      tabs[tabFocus].focus();
+      var k = ev.which || ev.keyCode;
+
+      // if the key pressed was an arrow key
+      if (k >= LEFT_ARROW && k <= DOWN_ARROW){
+        // move left one tab for left and up arrows
+        if (k == LEFT_ARROW || k == UP_ARROW){
+          if (index > 0) {
+            index--;
+          }
+          // unless you are on the first tab,
+          // in which case select the last tab.
+          else {
+            index = $tabs.length - 1;
+          }
+        }
+
+        // move right one tab for right and down arrows
+        else if (k == RIGHT_ARROW || k == DOWN_ARROW){
+          if (index < ($tabs.length - 1)){
+            index++;
+          }
+          // unless you're at the last tab,
+          // in which case select the first one
+          else {
+            index = 0;
+          }
+        }
+
+        // trigger a click event on the tab to move to
+        $($tabs.get(index)).click();
+        ev.preventDefault();
+      }
     }
   });
-});
+
 
 function changeTabs(e) {
   const target = e.target;
@@ -937,4 +954,3 @@ function changeTabs(e) {
     .removeAttribute("hidden");
 }
  
-
