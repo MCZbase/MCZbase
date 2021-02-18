c6980c72a181b40719aa78917405283b2a4677bb
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-02-18 13:41:25 -0500

    ISSUE  687  PUROSE:  Specimen details styles  DESC: ID

--- specimens/js/specimens.js
+++ specimens/js/specimens.js
@@ -86,7 +86,7 @@ targetDiv="CitPubFormMedia";
 //	});
 //};
 function loadIdentification(identification_id,form) {
-	$("#dialog_identification").dialog( "option", "title", "Edit Identification " + identification_id );
+	$("#dialog_identification").dialog( "option", "title", "Edit Identification here:" + identification_id );
 	$("#identificationForm").html(""); 
 	jQuery.getJSON("/transactions/component/functions.cfc",
 		{
