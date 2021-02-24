85ce4563b024b9bdb734a96e1a60add97c2772a5
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-02-24 15:35:20 -0500

    Issue 754  Keyboard and accessible navigation for tabs on search pages (transactions, specimens) DESC:  fix arrow navigation

--- shared/js/tabs.js
+++ shared/js/tabs.js
@@ -4,7 +4,6 @@
 window.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll('[role="tab"]');
   const tabList = document.querySelector('[role="tablist"]');
-  const tabActive = document.querySelector('[tabindex="0"]').focus;
 
   // Add a click event handler to each tab
   tabs.forEach(tab => {
@@ -12,7 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   // Enable arrow navigation between tabs in the tab list
-  let tabFocus = tabActive;
+  let tabFocus = document.querySelector('[tabindex="0"]');
 
    tabList.addEventListener("keydown", e => {
     // Move right
