27d5e1d7d307050c80401127ad034e1244a6b6b3
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-03-29 19:06:37 -0400

    ISSUE: 765  SpecimenDetails: edit dialogs DESC: something broke when working on locality ID -- back tracking.

--- specimens/js/specimens.js
+++ specimens/js/specimens.js
@@ -200,39 +200,39 @@ function loadParts(collection_object_id,targetDivId) {
 	});
 }
 
-function loadLocality(collection_object_id,targetDivId) { 
-	jQuery.ajax({
-		url: "/specimens/component/public.cfc",
-		data : {
-			method : "getLocalityHTML",
-			collection_object_id: collection_object_id,
-		},
-		success: function (result) {
-			$("#" + targetDivId ).html(result);
-		},
-		error: function (jqXHR, textStatus, error) {
-			handleFail(jqXHR,textStatus,error,"loading locality");
-		},
-		dataType: "html"
-	});
-}
+//function loadLocality(collection_object_id,targetDivId) { 
+//	jQuery.ajax({
+//		url: "/specimens/component/public.cfc",
+//		data : {
+//			method : "getLocalityHTML",
+//			collection_object_id: collection_object_id,
+//		},
+//		success: function (result) {
+//			$("#" + targetDivId ).html(result);
+//		},
+//		error: function (jqXHR, textStatus, error) {
+//			handleFail(jqXHR,textStatus,error,"loading locality");
+//		},
+//		dataType: "html"
+//	});
+//}
 
-function loadLocality(collecting_event_id,form) { 
-	jQuery.ajax({
-		url: "/specimens/component/functions.cfc",
-		data : {
-			method : "getLocalityHtml",
-			collecting_event_id: collecting_event_id,
-		},
-		success: function (result) {
-			$("#localityHTML).html(result);
-		},
-		error: function (jqXHR, textStatus, error) {
-			handleFail(jqXHR,textStatus,error,"loading locality");
-		},
-		dataType: "html"
-	});
-}
+//function loadLocality(collecting_event_id,form) { 
+//	jQuery.ajax({
+//		url: "/specimens/component/functions.cfc",
+//		data : {
+//			method : "getLocalityHtml",
+//			collecting_event_id: collecting_event_id,
+//		},
+//		success: function (result) {
+//			$("#localityHTML).html(result);
+//		},
+//		error: function (jqXHR, textStatus, error) {
+//			handleFail(jqXHR,textStatus,error,"loading locality");
+//		},
+//		dataType: "html"
+//	});
+//}
 /** openEditIdentificationsDialog (plural) open a dialog for editing 
  * identifications for a cataloged item.
  * @param collection_object_id for the cataloged_item for which to edit identifications.
