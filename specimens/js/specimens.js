395426ab85991f77af491cd16824a36d90b2efe0
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-04-05 10:03:43 -0400

    ISSUE: 765  SpecimenDetails: edit dialogs DESC: Identification

--- specimens/js/specimens.js
+++ specimens/js/specimens.js
@@ -1,126 +1,3 @@
-function loadCitPubForMedia(publication_id) {
-targetDiv="CitPubFormMedia";
-	console.log(" media in #"+ targetDiv);
-	jQuery.ajax({
-		url: "/specimens/component/functions.cfc",
-		data : {
-			method : "getMediaForCitPub",
-			publication_id: publication_id,
-		},
-		success: function (result) {
-			$("#CitPubFormMedia").html(result);
-		},
-		error: function (jqXHR, textStatus, error) {
-			handleFail(jqXHR,textStatus,error,"removing pub");
-		},
-		dataType: "html"
-	});
-}
-
-	$(function() {
-     $(".dialog").dialog({
-		open: function(event,ui){},
-        Title: {style:"font-size: 1.3em;"},
-		bgiframe: true,
-        autoOpen: false,
-    	width: '700px',
-    	minWidth: 500,
-    	minHeight: 400,
-		buttons: [
-			{ text: "Cancel", click: function () { $(this).dialog( "close" );}, class: "btn", style:"background: none; border: none;" },
-        	{ text: "Save",  click: function() { alert("save"); }, class:"btn btn-primary"}
-		 ],
-        close: function() {
-            $(this).dialog( "close" );
-        },
-        modal: true
-	 }
-       );
-     $('body')
-      .bind(
-       'click',
-       function(e){
-        if(
-         $('.dialog-ID').dialog('isOpen')
-         && !$(e.target).is('.ui-dialog, button')
-         && !$(e.target).closest('.ui-dialog').length
-        ){
-         $('.dialog').dialog('close');
-        }
-       }
-      );
-    }
-   );
-
-
-	$(function() {
-     $(".dialog-locality").dialog({
-		open: function(event,ui){},
-        Title: {style:"font-size: 1.3em;"},
-		bgiframe: true,
-        autoOpen: false,
-    	width: '700px',
-    	minWidth: 500,
-    	minHeight: 400,
-		buttons: [
-			{ text: "Cancel", click: function () { $(this).dialog( "close" );}, class: "btn", style:"background: none; border: none;" },
-        	{ text: "Save",  click: function() { alert("save"); }, class:"btn btn-primary"}
-		 ],
-        close: function() {
-            $(this).dialog( "close" );
-        },
-        modal: true
-	 }
-       );
-     $('body')
-      .bind(
-       'click',
-       function(e){
-        if(
-         $('.dialog-ID').dialog('isOpen')
-         && !$(e.target).is('.ui-dialog, button')
-         && !$(e.target).closest('.ui-dialog').length
-        ){
-         $('.dialog-locality').dialog('close');
-        }
-       }
-      );
-    }
-   );
-function loadIdentification(identification_id,form) {
-	jQuery.ajax({
-		url: "/specimens/component/functions.cfc",
-		data : {
-			method : "getIdentificationHtml",
-			identification_id: identification_id,
-		},
-		success: function (result) {
-			$("#identificationHTML").html(result);
-		},
-		error: function (jqXHR, textStatus, error) {
-			handleFail(jqXHR,textStatus,error,"removing identification");
-		},
-		dataType: "html"
-	});
-};
-
-function loadLocality(locality_id,form) {
-	jQuery.ajax({
-		url: "/specimens/component/functions.cfc",
-		data : {
-			method : "getLocalityHtml",
-			locality_id: locality_id,
-		},
-		success: function (result) {
-			$("#localityHTML").html(result);
-		},
-		error: function (jqXHR, textStatus, error) {
-			handleFail(jqXHR,textStatus,error,"removing locality");
-		},
-		dataType: "html"
-	});
-};
-
 function checkFormValidity(form) { 
 	var result = false;
 	if (!form.checkValidity || form.checkValidity()) { 
@@ -142,13 +19,32 @@ function checkFormValidity(form) {
 };
 
 /** loadIdentifications populate an html block with the identification 
- * history for a cataloged item.
- * @param collection_object_id identifying the cataloged item for which 
- *  to list the identification history.
- * @param targetDivId the id for the div in the dom, without a leading #
- *  selector, for which to replace the html content with the identification 
- *  history.
- */
+* history for a cataloged item.
+* @param collection_object_id identifying the cataloged item for which 
+*  to list the identification history.
+* @param targetDivId the id for the div in the dom, without a leading #
+*  selector, for which to replace the html content with the identification 
+*  history.
+*/
+
+function loadIdentifications(identification_id,form) {
+	jQuery.ajax({
+		url: "/specimens/component/functions.cfc",
+		data : {
+			method : "getIdentificationsHTML",
+			identification_id: identification_id,
+		},
+		success: function (result) {
+			$("#identificationsHTML").html(result);
+		},
+		error: function (jqXHR, textStatus, error) {
+			handleFail(jqXHR,textStatus,error,"removing identification");
+		},
+		dataType: "html"
+	});
+};
+
+
 function loadIdentifications(collection_object_id,targetDivId) { 
 	jQuery.ajax({
 		url: "/specimens/component/public.cfc",
@@ -165,7 +61,39 @@ function loadIdentifications(collection_object_id,targetDivId) {
 		dataType: "html"
 	});
 }
+function loadCitations(collection_object_id,targetDivId) { 
+	jQuery.ajax({
+		url: "/specimens/component/public.cfc",
+		data : {
+			method : "getCitationsHTML",
+			collection_object_id: collection_object_id,
+		},
+		success: function (result) {
+			$("#" + targetDivId ).html(result);
+		},
+		error: function (jqXHR, textStatus, error) {
+			handleFail(jqXHR,textStatus,error,"loading citations");
+		},
+		dataType: "html"
+	});
+}
 
+function loadParts(collection_object_id,targetDivId) { 
+	jQuery.ajax({
+		url: "/specimens/component/public.cfc",
+		data : {
+			method : "getPartsHTML",
+			collection_object_id: collection_object_id,
+		},
+		success: function (result) {
+			$("#" + targetDivId ).html(result);
+		},
+		error: function (jqXHR, textStatus, error) {
+			handleFail(jqXHR,textStatus,error,"loading parts");
+		},
+		dataType: "html"
+	});
+}
 /** openEditIdentificationsDialog (plural) open a dialog for editing 
  * identifications for a cataloged item.
  * @param collection_object_id for the cataloged_item for which to edit identifications.
@@ -188,6 +116,56 @@ function openEditIdentificationsDialog(collection_object_id,dialogId,guid,callba
 		},
 		error: function (jqXHR, textStatus, error) {
 			handleFail(jqXHR,textStatus,error,"opening edit identifications dialog");
+		},
+		dataType: "html"
+	});
+};
+/** loadIdentifications populate an html block with the identification 
+ * history for a cataloged item.
+ * @param collection_object_id identifying the cataloged item for which 
+ *  to list the identification history.
+ * @param targetDivId the id for the div in the dom, without a leading #
+ *  selector, for which to replace the html content with the identification 
+ *  history.
+ */
+function loadOtherIDs(collection_object_id,targetDivId) { 
+	jQuery.ajax({
+		url: "/specimens/component/public.cfc",
+		data : {
+			method : "getOtherIDsHTML",
+			collection_object_id: collection_object_id,
+		},
+		success: function (result) {
+			$("#" + targetDivId ).html(result);
+		},
+		error: function (jqXHR, textStatus, error) {
+			handleFail(jqXHR,textStatus,error,"loading other ids");
+		},
+		dataType: "html"
+	});
+}
+/** openEditOtherIDsDialog (plural) open a dialog for editing 
+ * identifications for a cataloged item.
+ * @param collection_object_id for the cataloged_item for which to edit identifications.
+ * @param dialogId the id in the dom for the div to turn into the dialog without 
+ *  a leading # selector.
+ * @param guid the guid of the specimen to display in the dialog title
+ * @param callback a callback function to invoke on closing the dialog.
+ */
+function openEditOtherIDsDialog(collection_object_id,dialogId,guid,callback) {
+	var title = "Edit Other IDs for " + guid;
+	createSpecimenEditDialog(dialogId,title,callback);
+	jQuery.ajax({
+		url: "/specimens/component/functions.cfc",
+		data : {
+			method : "getEditOtherIDsHTML",
+			collection_object_id: collection_object_id,
+		},
+		success: function (result) {
+			$("#" + dialogId + "_div").html(result);
+		},
+		error: function (jqXHR, textStatus, error) {
+			handleFail(jqXHR,textStatus,error,"opening edit Other IDs dialog");
 		},
 		dataType: "html"
 	});
@@ -216,7 +194,7 @@ function createSpecimenEditDialog(dialogId,title,closecallback) {
 		w = Math.floor(w *.9);
 	} else if (w>1333) { 
 		// cap width at 1200 pixel
-		w = 1200;
+		w = 999;
 	} 
 	var thedialog = $("#"+dialogId).html(content)
 	.dialog({
