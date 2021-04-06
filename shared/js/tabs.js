15f832cfbfa0fd37db871ff5f2b423fbd763a6a4
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-04-06 10:47:54 -0400

    ISSUE 754  Assigning tab focus for keyboard control

--- shared/js/tabs.js
+++ shared/js/tabs.js
@@ -6,9 +6,10 @@ window.addEventListener("DOMContentLoaded", () => {
   const tabList = document.querySelector('[role="tablist"]');
      var tab = $('.tabList > .active').get(0);        
         alert('Hello from Tab Button ' + $(tab).attr('id'));
+	
   // Add a click event handler to each tab
   tabs.forEach(tab => {
-    tab.addEventListener("click", changeTabs);
+    tab.addEventListener("click", changeTabs,0);
   });
 
   // Enable arrow navigation between tabs in the tab list
