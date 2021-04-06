8cafd21a1fbaecebd927fadfcc916b5ca49c5340
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-04-06 12:09:03 -0400

    ISSUE 754  Assigning tab focus for keyboard control

--- shared/js/tabs.js
+++ shared/js/tabs.js
@@ -2,65 +2,64 @@
 // from https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Tab_Role#example 
 //
 window.addEventListener("DOMContentLoaded", () => {
-  const tabs = document.querySelectorAll('[role="tab"]');
-  const tabList = document.querySelector('[role="tablist"]');
-     var tab = $('.tabList > .active').get(0);        
-        alert('Hello from Tab Button ' + $(tab).attr('id'));
-	
-  // Add a click event handler to each tab
-  tabs.forEach(tab => {
-    tab.addEventListener("click", changeTabs); 
+	const tabs = document.querySelectorAll('[role="tab"]');
+	const tabList = document.querySelector('[role="tablist"]');
+	var tab = $('.tabList > .active').get(0);
+	alert('Hello from Tab Button ' + $(tab).attr('id'));
+
+// Add a click event handler to each tab
+	tabs.forEach(tab => {
+		tab.addEventListener("click", changeTabs); 
 		tab.focus(0);
-  });
+	});
 
-  // Enable arrow navigation between tabs in the tab list
- // let tabFocus = 0; // "0" is a problem when any page other than "all transactions" is selected. Arrow right and left start at the first tab.
-let tabFocus = 0;
-   tabList.addEventListener("keydown", e => {
-    // Move right
-    if (e.keyCode === 39 || e.keyCode === 37) {
-      tabs[tabFocus].setAttribute("tabindex", -1);
-      if (e.keyCode === 39) {
-        tabFocus++;
-        // If we're at the end, go to the start
-        if (tabFocus >= tabs.length) {
-          tabFocus = 0;
-        }
-        // Move left
-      } else if (e.keyCode === 37) {
-        tabFocus--;
-        // If we're at the start, move to the end
-        if (tabFocus < 0) {
-          tabFocus = tabs.length - 1;
-        }
-      }
-
-      tabs[tabFocus].setAttribute("tabindex", 0);
-      tabs[tabFocus].focus();
-    }
-  });
+// Enable arrow navigation between tabs in the tab list
+// let tabFocus = 0; // "0" is a problem when any page other than "all transactions" is selected. Arrow right and left start at the first tab.
+	let tabFocus = 0;
+	tabList.addEventListener("keydown", e => {
+		// Move right
+		if (e.keyCode === 39 || e.keyCode === 37) {
+			tabs[tabFocus].setAttribute("tabindex", -1);
+			if (e.keyCode === 39) {
+				tabFocus++;
+				// If we're at the end, go to the start
+				if (tabFocus >= tabs.length) {
+					tabFocus = 0;
+				}
+				// Move left
+			} else if (e.keyCode === 37) {
+				tabFocus--;
+				// If we're at the start, move to the end
+				if (tabFocus < 0) {
+					tabFocus = tabs.length - 1;
+				}
+			}
+			tabs[tabFocus].setAttribute("tabindex", 0);
+			tabs[tabFocus].focus();
+		}
+	});
 });
 
 function changeTabs(e) {
-  const target = e.target;
-  const parent = target.parentNode;
-  const grandparent = parent.parentNode;
+	const target = e.target;
+	const parent = target.parentNode;
+	const grandparent = parent.parentNode;
 
-  // Remove all current selected tabs
-  parent
-    .querySelectorAll('[aria-selected="true"]')
-    .forEach(t => t.setAttribute("aria-selected", false));
+	// Remove all current selected tabs
+	parent
+		.querySelectorAll('[aria-selected="true"]')
+		.forEach(t => t.setAttribute("aria-selected", false));
 
-  // Set this tab as selected
-  target.setAttribute("aria-selected", true);
+	// Set this tab as selected
+	target.setAttribute("aria-selected", true);
 
-  // Hide all tab panels
-  grandparent
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach(p => p.setAttribute("hidden", true));
+	// Hide all tab panels
+	grandparent
+	.querySelectorAll('[role="tabpanel"]')
+	.forEach(p => p.setAttribute("hidden", true));
 
-  // Show the selected panel
-  grandparent.parentNode
-    .querySelector(`#${target.getAttribute("aria-controls")}`)
-    .removeAttribute("hidden");
-}
+	// Show the selected panel
+	grandparent.parentNode
+		.querySelector(`#${target.getAttribute("aria-controls")}`)
+		.removeAttribute("hidden");
+	}
