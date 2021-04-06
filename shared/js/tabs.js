fb262445d3dbd89402e67b7ace6b9208d6a15a80
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-04-06 11:53:30 -0400

    ISSUE 754  Assigning tab focus for keyboard control

--- shared/js/tabs.js
+++ shared/js/tabs.js
@@ -14,8 +14,8 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   // Enable arrow navigation between tabs in the tab list
-  let tabFocus = 0; // "0" is a problem when any page other than "all transactions" is selected. Arrow right and left start at the first tab.
-
+ // let tabFocus = 0; // "0" is a problem when any page other than "all transactions" is selected. Arrow right and left start at the first tab.
+var tabFocus = '';
    tabList.addEventListener("keydown", e => {
     // Move right
     if (e.keyCode === 39 || e.keyCode === 37) {
