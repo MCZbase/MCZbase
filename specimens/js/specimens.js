1b85f4cca094069b6f9e2943fb5f97dcce8babe7
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-06-07 10:51:11 -0400

    ISSUE: 765  SpecimenDetails: edit dialogs DESC: Edit Dialog Media

--- specimens/js/specimens.js
+++ specimens/js/specimens.js
@@ -318,14 +318,14 @@ function openEditMediaDialog(collection_object_id,dialogId,guid,callback) {
 		dataType: "html"
 	});
 };
-function openEditMediaDetailsDialog(collection_object_id,dialogId,guid,callback) {
-	var title = "Edit Media for " + guid;
+function openEditMediaDetailsDialog(media_id,dialogId,callback) {
+	var title = "Edit Media";
 	createSpecimenEditDialog(dialogId,title,callback);
 	jQuery.ajax({
 		url: "/specimens/component/functions.cfc",
 		data : {
 			method : "getEditMediaDetailsHTML",
-			collection_object_id: collection_object_id,
+			media_id: media_id,
 		},
 		success: function (result) {
 			$("#" + dialogId + "_div").html(result);
