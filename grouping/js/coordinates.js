734a3f0f8d3666ab319ebc14965f4edb4e06f63a
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-08-26 13:37:53 -0400

    ISSUE 696:  Named Grouping  PURPOSE: Styling of heat map

--- grouping/js/coordinates.js
+++ grouping/js/coordinates.js
@@ -1,10 +1,7 @@
 // JavaScript Document
 
-var getMapData = function(cfmapname, cfmaplatitude, cfmaplongitude, cfmapaddress){
+function getMapData(cfmaplatitude, cfmaplongitude){
 var msg = "";
-msg = msg + "Map Name: " + cfmapname + "<br>";
 msg = msg + "Latitude,longitude: " + "(" + cfmaplatitude + "," + cfmaplongitude + ")" + "<br>";
-msg = msg + "Address: " + cfmapaddress + "<br>";
-//alert(msg);
 return "<br><table><tr><td bgcolor='red'><h4><font color='white'>" + "Javascript Bind Example" + "</font></td></tr></table><hr>" + msg;
 }
