664501f98026908276aa1f1fd83ddbd3e1863511
chicoreus <mole@morris.net>
2021-09-28 19:58:55 -0400

    ISSUE: Redmine 581 Redmine 546 PURPOSE: Specimen search/results supporting metadata management DESCRIPTION: adding autocomplete backing methods and javascript functions, binding to search fields for specimen search field and specimen search result column metadata pages, bugfix to 4aad5bb25 fixing parameter name in method call.

--- specimens/js/specimens.js
+++ specimens/js/specimens.js
@@ -1064,7 +1064,7 @@ function makeSpecResColsAutocomplete(inputId, targetField) {
 		source: function (request, response) {
 			$.ajax({
 				url: "/specimens/component/search.cfc",
-				data: { term: request.term, method: 'getSpecResColsAutocomplete', rank: targetField },
+				data: { term: request.term, method: 'getSpecResColsAutocomplete', field: targetField },
 				dataType: 'json',
 				success : function (data) { response(data); },
 				error : function (jqXHR, textStatus, error) {
@@ -1092,7 +1092,7 @@ function makeSpecSearchColsAutocomplete(inputId, targetField) {
 		source: function (request, response) {
 			$.ajax({
 				url: "/specimens/component/search.cfc",
-				data: { term: request.term, method: 'getSpecSearchColsAutocomplete', rank: targetField },
+				data: { term: request.term, method: 'getSpecSearchColsAutocomplete', field: targetField },
 				dataType: 'json',
 				success : function (data) { response(data); },
 				error : function (jqXHR, textStatus, error) {
