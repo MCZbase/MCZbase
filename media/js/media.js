c6f841c98ad27e200d00f96b5f86eee55639ddea
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2021-12-01 20:49:20 -0500

    Create MediaGallery.cfm page for testing widget

--- media/js/media.js
+++ media/js/media.js
@@ -230,7 +230,7 @@ function makeAnyMediaRelationAutocomplete(valueControl,typeControl,idControl) {
 
 function getMediaBlockHtml(media_id) {
 	jQuery.ajax({
-		url: "/media/component/shared.cfc",
+		url: "/media/component/function.cfc",
 		data : {
 			method : "getMediaBlockHtml",
 			media_id: media_id,
