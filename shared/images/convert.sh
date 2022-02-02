c90719bde647fd5730f1203a4155c370284d25b7
chicoreus <mole@morris.net>
2022-02-02 16:11:00 -0500

    ISSUE: PURPOSE: Rescale external thumbnails, failing gracefully. DESCRIPTION: cleaner failover .png images, removing generation script.

--- shared/images/convert.sh
+++
@@ -1,7 +0,0 @@
-gm convert -resize 150x Image-x-generic.svg Image-x-generic.png
-gm convert -resize 150x Gnome-audio-volume-medium.svg Gnome-audio-volume-medium.png
-gm convert -resize 150x Gnome-media-playback-start.svg Gnome-media-playback-start.png
-gm convert -resize 150x Gnome-text-x-generic.svg Gnome-text-x-generic.png
-gm convert -resize 150x model_3d.svg model_3d.png
-gm convert -resize 150x Sine_waves_different_frequencies.svg Sine_waves_different_frequencies.png
-gm convert -resize 150x Image-x-generic.svg Image-x-generic.png
