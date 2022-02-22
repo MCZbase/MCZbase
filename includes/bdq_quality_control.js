50a01315d64dd4e30120a1195dc6a4e1b56bf13a
chicoreus <mole@morris.net>
2022-02-22 14:55:08 -0500

    ISSUE: Redmine 655 Redmine 868 PURPOSE: Adding support for TDWG BDQ TG2 Space tests.  DESCRIPTION: Bugfix copy/paste error, space tests don't show None for no ammendments.

--- includes/bdq_quality_control.js
+++ includes/bdq_quality_control.js
@@ -391,6 +391,7 @@ console.log(postkey);
 
 				// Iterate through amendments (would need to obtain acted upon/consulted annotations on terms to fully present as changes to terms).
 				// Could extract change terms from values and present in term centric rather than test centric view.
+				var amendmentCount = 0;
 				for (var k in amend) { 
 					var key = amend[k];
 					if (key.status == 'FILLED_IN') { 		
@@ -401,6 +402,10 @@ console.log(postkey);
   						cs=""; ce="";
 					}
 					displayamendments = displayamendments + "<li><span>" + key.label + " " + key.status + " " + cs + key.value + ce + " " + key.comment + "</span></li>";
+					amendmentCount++;
+				}
+				if (amendmentCount==0) { 
+					displayamendments = displayamendments + "<li><span>None</span></li>";
 				}
 
 				// Iterate through post-amendment tests to calculate postpass.
