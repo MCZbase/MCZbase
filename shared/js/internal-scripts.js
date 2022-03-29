bed9806bd642d679fb4501b76bb8268988611aaa
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2022-03-29 18:01:42 -0400

    Redmine Issue Redesign #865: 'Edit Media' page style; DESC: class list

--- shared/js/internal-scripts.js
+++ shared/js/internal-scripts.js
@@ -254,7 +254,7 @@ function opencreatemediadialog(dialogid, related_value, related_id, relationship
 function addRelation (n,targetId) {
 	var pDiv=document.getElementById(targetId);
 	var nDiv = document.createElement('div');
-	nDiv.classList='relationshipDiv__' + n;
+	nDiv.classList='form-row col-12 px-0 mx-0 relationshipDiv__' + n;
 	pDiv.appendChild(nDiv);
 	var n1=n-1;
 
