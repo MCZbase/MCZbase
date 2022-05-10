739c1f15f3bfefaccb88f9b0b7c4a0cc8f91f363
chicoreus <mole@morris.net>
2022-05-10 11:02:18 -0400

    ISSUE: Redmine 883 PURPOSE: Specimen search results details dialog with links out to resources. DESCRIPTION: tuning 8d205c30b, link to search for specimens by taxon.

--- specimens/js/search.js
+++ specimens/js/search.js
@@ -41,7 +41,7 @@ function createSpecimenRowDetailsDialog(gridId, rowDetailsTargetId, datarecord,r
 				content = content + "<li><strong>" + text + ":</strong> <a href='/name/"+scientific_name+"' target='_blank'>" + datarecord[datafield] +  "</a></li>";
 			} else if (datafield=="SCI_NAME_WITH_AUTH") { 
 				content = content + "<li><strong>" + text + ":</strong> <a href='/name/"+scientific_name+"' target='_blank'>" + datarecord[datafield] +  "</a></li>";
-				content = content + "<li><strong>" + text + ":</strong> <a href='/Specimens.cfm?action=fixedSearch&scientific_name="+scientific_name+"' target='_blank'> Search for specimens identified as " + datarecord[datafield] +  "</a></li>";
+				content = content + "<li>Search for <a href='/Specimens.cfm?action=fixedSearch&execute=true&scientific_name="+scientific_name+"' target='_blank'>specimens identified as " + datarecord[datafield] +  "</a></li>";
 			} else if (datafield=="MEDIA") { 
 				content = content + "<li><strong>" + text + ":</strong> <a href='/media/findMedia.cfm?execute=true&method=getMedia&media_relationship_type=ANY%20cataloged_item&media_relationship_value="+ guid +"&media_relationship_id=" + collection_object_id + "' aria-label='related media' target='_blank'>" + datarecord[datafield] +  "</a></li>";
 			} else if (datafield=="GUID") { 
