997099d1745d74926cb71e74df7f32988ff0b360
chicoreus <mole@morris.net>
2022-05-10 10:58:09 -0400

    ISSUE: Redmine 883 PURPOSE: Specimen search results details dialog with links out to resources. DESCRIPTION: Bugfix to 8d205c30b, wrong operator for comparison.

--- specimens/js/search.js
+++ specimens/js/search.js
@@ -46,7 +46,7 @@ function createSpecimenRowDetailsDialog(gridId, rowDetailsTargetId, datarecord,r
 				content = content + "<li><strong>" + text + ":</strong> <a href='/media/findMedia.cfm?execute=true&method=getMedia&media_relationship_type=ANY%20cataloged_item&media_relationship_value="+ guid +"&media_relationship_id=" + collection_object_id + "' aria-label='related media' target='_blank'>" + datarecord[datafield] +  "</a></li>";
 			} else if (datafield=="GUID") { 
 				content = content + "<li><strong>" + text + ":</strong> <a href='/guid/"+guid+"' target='_blank'>" + datarecord[datafield] +  "</a></li>";
-			} else if (datafield=="CAT_NUM_INGEGER" || datafield=="CAT_NUM_PREFIX" || datafield="CAT_NUM_SUFFIX") {
+			} else if (datafield=="CAT_NUM_INGEGER" || datafield=="CAT_NUM_PREFIX" || datafield=="CAT_NUM_SUFFIX") {
 				// skip 
 			} else { 
 				content = content + "<li><strong>" + text + ":</strong> " + datarecord[datafield] +  "</li>";
