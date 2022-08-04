6af967dfe1a03835fb1a30c965e5ce54076877c1
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2022-08-04 14:09:06 -0400

    Redmine Issue: 687 Specimen Details Style PURPOSE: condition history

--- specimens/js/public.js
+++ specimens/js/public.js
@@ -1124,7 +1124,7 @@ function localityMapSetup(){
   */
 function openHistoryDialog(collection_object_id, dialogid) { 
 	var title = "Part Preparation and Condition History.";
-	var content = '<div id="'+dialogid+'_div" class="col-12 px-5">Loading....</div>';
+	var content = '<div id="'+dialogid+'_div" class="col-12 px-1 px-xl-5">Loading....</div>';
 	var thedialog = $("#"+dialogid).html(content)
 	.dialog({
 		title: title,
