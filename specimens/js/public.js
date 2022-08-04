16175f20f318b51ae544b811948efc65d93926c4
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2022-08-04 13:38:05 -0400

    Redmine Issue: 687 Specimen Details Style PURPOSE: condition history

--- specimens/js/public.js
+++ specimens/js/public.js
@@ -1134,7 +1134,7 @@ function openHistoryDialog(collection_object_id, dialogid) {
 		stack: true,
 		height: 'auto',
 		width: '90%',
-		minWidth: 600,
+		maxWidth: 600,
 		minHeight: 500,
 		draggable:true,
 		buttons: {
