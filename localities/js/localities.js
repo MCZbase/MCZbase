97861c277bcaba7bcfe1e38d3951b0eef00a75ad
chicoreus <mole@morris.net>
2023-04-08 22:44:14 -0400

    Bugfix ISSUE: Redmine 831 PURPOSE: create/edit locality DESCRIPTION: return from ajax before lookup completes, passing target to function.

--- localities/js/localities.js
+++ localities/js/localities.js
@@ -1,5 +1,12 @@
 
-function suggestSovereignNation(geog_auth_rec_id) {
+/** given a geog_auth_rec_id, look up a plausible value for sovereign nation
+ and paste it into a control.
+ @param geog_auth_rec_id the higher geography record from which to suggest 
+   a sovereign nation value based on the country in the higher geography.
+ @param pasteTarget the id in the dom, without a leading pound selector
+   into which to paste the suggestion if any.
+**/
+function suggestSovereignNation(geog_auth_rec_id, pasteTarget) {
    jQuery.getJSON("/localities/component/search.cfc",
       {
          method : "suggestSovereignNation",
@@ -10,7 +17,11 @@ function suggestSovereignNation(geog_auth_rec_id) {
       function (result) {
 			console.log(result);
 			if (result && result[0]) { 
-				return(result[0].id);
+				var suggestion = result[0].id;
+				console.log(suggestion);
+				if (suggestion) { 
+					$("##"+pasteTarget).val(suggestion);
+				}
 			}
       }
    ).fail(function(jqXHR,textStatus,error){
