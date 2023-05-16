59fb45adc2be928243a8d002377f1eee4f2b4d1a
chicoreus <mole@morris.net>
2023-05-16 17:49:54 -0400

    ISSUE: Redmine: 830 PURPOSE: Create/Edit Higher Geography. DESCRIPTION: Bugfix to de11dbc41 removing coldfusion escaping for # from javascript file not interpreted by coldfusion.

--- localities/js/highergeography.js
+++ localities/js/highergeography.js
@@ -7,28 +7,28 @@
 */
 function getLowestGeography() {
 	var	result = "";
-	if ($('##island').val()!="") {
-		result = $('##island').val();
-	} else if ($('##island_group').val()!="") {
-		result = $('##island_group').val();
-	} else if ($('##feature').val()!="") {
-		result = $('##feature').val();
-	} else if ($('##county').val()!="") {
-		result = $('##county').val();
-	} else if ($('##state_prov').val()!="") {
-		result = $('##state_prov').val();
-	} else if ($('##country').val()!="") {
-		result = $('##country').val();
-	} else if ($('##water_feature').val()!="") {
-		result = $('##water_feature').val();
-	} else if ($('##sea').val()!="") {
-		result = $('##sea').val();
-	} else if ($('##ocean_subregion').val()!="") {
-		result = $('##ocean_subregion').val();
-	} else if ($('##ocean_region').val()!="") {
-		result = $('##ocean_region').val();
-	} else if ($('##continent_ocean').val()!="") {
-		result = $('##continent_ocean').val();
+	if ($('#island').val()!="") {
+		result = $('#island').val();
+	} else if ($('#island_group').val()!="") {
+		result = $('#island_group').val();
+	} else if ($('#feature').val()!="") {
+		result = $('#feature').val();
+	} else if ($('#county').val()!="") {
+		result = $('#county').val();
+	} else if ($('#state_prov').val()!="") {
+		result = $('#state_prov').val();
+	} else if ($('#country').val()!="") {
+		result = $('#country').val();
+	} else if ($('#water_feature').val()!="") {
+		result = $('#water_feature').val();
+	} else if ($('#sea').val()!="") {
+		result = $('#sea').val();
+	} else if ($('#ocean_subregion').val()!="") {
+		result = $('#ocean_subregion').val();
+	} else if ($('#ocean_region').val()!="") {
+		result = $('#ocean_region').val();
+	} else if ($('#continent_ocean').val()!="") {
+		result = $('#continent_ocean').val();
 	}
 	return	result;
 }
