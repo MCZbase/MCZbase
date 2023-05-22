9c9d0f6ae4e56555e672e6dcae2b2b2fd0d5ddb5
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2023-05-22 14:18:32 -0400

    Redesign #831: Redesign edit locality/collecting event pages; styling pages

--- localities/js/localities.js
+++ localities/js/localities.js
@@ -168,7 +168,7 @@ function openEditGeorefDialog(lat_long_id, dialogid, callback) {
 		modal: true,
 		stack: true,
 		zindex: 2000,
-		height: 700,
+		height: 705,
 		width: w,
 		minWidth: 600,
 		minHeight: 500,
