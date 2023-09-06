90920ccce1df3cbbc44a76d664f718726c95a961
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2023-09-06 09:09:22 -0400

    Redmine 928; Test visualization code plan

---
+++ metrics/js/metrics.js
@@ -0,0 +1,17 @@
+function getMetrics(targetDiv) { 
+	console.log("Where is it? " + targetDiv);
+	jQuery.ajax({
+		url: "/media/component/functions.cfc",
+		data : {
+			method : "getMetrics",
+	
+		},
+		success: function (result) {
+			$("#" + targetDiv).html(result);
+		},
+		error: function (jqXHR, textStatus, error) {
+			handleFail(jqXHR,textStatus,error,"retrieving metadata block");
+		},
+		dataType: "html"
+	});
+};
