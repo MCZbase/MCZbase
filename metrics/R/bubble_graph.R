9b4b6aed3407898d97e9cfedf6dc61e23250aa25
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2023-09-06 14:32:16 -0400

    Redmine 928; Test visualization code plan

--- metrics/R/bubble_graph.R
+++ metrics/R/bubble_graph.R
@@ -20,10 +20,13 @@ Cit3 <- filter(Chart_Data_Img_per_year, ENTERED_DATE >= 1800)
 
 
 BubbleAnim <- ggplot(Cit3, aes(NUMBER_CATALOG_ITEMS, NUMBER_OF_IMAGES, shape = NUMBER_OF_TYPES_WITH_IMAGES, 
-              colour = TYPE_STATUS, guide_colourbar(title = "Types with Images" ))) +
+  colour = TYPE_STATUS, guide_colourbar(title = "Types with Images" ))) +
   geom_point(size=5,alpha = 0.9, show.legend = TRUE) +
   scale_fill_gradient2(low="#075AFF",mid="#ffffcc",high="#FF0000") +
   scale_shape_binned() +
+  labels2 <- c("no types", "50 types", "100 types", "150 types", "200 types")
+scale_shape_manual(values = 
+                     c(rep(1,5),0,1,2,3,4,5,6),labels= labels2)
  # scale_x_log10() +
  # facet_wrap(~CITATION_TYPE) +
   
