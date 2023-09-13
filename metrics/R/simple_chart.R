731211d1aeb0a451dc63a5aac678aeb74f1826d6
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2023-09-13 14:56:19 -0400

    Redmine Issue #934 Proof of concept R-Coldfusion integration; CFexecute to R

---
+++ metrics/R/simple_chart.R
@@ -0,0 +1,28 @@
+#graph 1: transitioning through time
+#animation set up
+library(gganimate)
+library(tibble)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(gapminder)
+library(ggthemes)
+library(gifski)
+library(png)
+library(ggthemes)
+
+#Chart_Data_Img_per_year <- read_csv('/metrics/datafiles/chart_data.csv')
+simple_chart <- `chart_data1`
+chart <- filter(simple_chart,COLLECTION=='Herpetology')
+chart0 <- filter(chart, CITATION_TYPE =='Primary')
+
+chart1 <- ggplot(chart0, aes(x=TYPE_STATUS, y=NUMBER_OF_CITATIONS, fill=TYPE_STATUS)) +
+  geom_bar(stat="identity",width = 1)+
+  coord_polar("y", start=0)
+chart1
+
+
+
+
+
