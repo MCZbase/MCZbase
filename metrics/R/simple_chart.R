d56ce3c51e50cb453478ed4961446b5d455bf990
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2023-09-14 16:42:10 -0400

    Redmine Issue #934 Proof of concept R-Coldfusion integration; CFexecute to R

--- metrics/R/simple_chart.R
+++ metrics/R/simple_chart.R
@@ -7,15 +7,12 @@ library(tidyr)
 
 
 simple_chart <- read_csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
-chart <- filter(simple_chart,COLLECTION=='Herpetology')
-chart0 <- filter(chart, CITATION_TYPE =='Primary')
 
-spec(chart0)
-#chart1 <- fortify(chart0, aes(x="", y=NUMBER_OF_CITATIONS, fill=TYPE_STATUS)) +
-  #geom_bar(stat="identity",width = 1)+
- # coord_polar("y", start=0)
-#print(chart1)
-#ggsave('/var/www/html/arctos/metrics/R/graphs/chart1.png',width=6, height=4,dpi=300)
+chart1 <- fortify(simple_chart, aes(x=NUMBER_OF_TYPES_WITH_IMAGES, y=NUMBER_OF_CITATIONS, fill=TYPE_STATUS)) +
+  geom_bar(stat="identity",width = 1)+
+  coord_polar("y", start=0)
+print(chart1)
+ggsave('/var/www/html/arctos/metrics/R/graphs/chart1.png',width=6, height=4,dpi=300)
 
 
 
