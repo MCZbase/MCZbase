15d37831b122c52931dd1aaf9477d0dc714b6857
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2023-09-14 16:59:55 -0400

    Redmine Issue #934 Proof of concept R-Coldfusion integration; CFexecute to R

--- metrics/R/simple_chart.R
+++ metrics/R/simple_chart.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(png)
 library(readr)
 library(tidyr)
-library(tidyverse)
+library(dplyr)
 
 
 simple_chart <- read_csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
