070f147e108bd0ba43f28b8e9ce0acb6e540851e
chicoreus <mole@morris.net>
2024-04-10 09:36:53 -0400

    ISSUE: BugID: 6318 PURPOSE: Show georeference count with search results. DESCRIPTION: Adding count to show georeferences button.

--- specimens/js/specimens.js
+++ specimens/js/specimens.js
@@ -15,7 +15,26 @@ function loadGeoreferenceSummaryHTML (result_id,targetDivId) {
 			$("#" + targetDivId ).html(result);
 		},
 		error: function (jqXHR, textStatus, error) {
-			handleFail(jqXHR,textStatus,error,"loading specimen identifiers");
+			handleFail(jqXHR,textStatus,error,"loading georeference summary");
+		},
+		dataType: "html"
+	});
+};
+
+/** Load information about georeferences in a result set into a target div.
+*/
+function loadGeoreferenceCount (result_id,targetDivId) { 
+	jQuery.ajax({
+		url: "/specimens/component/manage.cfc",
+		data : {
+			method : "getGeoreferenceCount",
+			result_id: result_id
+		},
+		success: function (result) {
+			$("#" + targetDivId ).html(result);
+		},
+		error: function (jqXHR, textStatus, error) {
+			handleFail(jqXHR,textStatus,error,"loading georeference count");
 		},
 		dataType: "html"
 	});
