a8f2ba953d79c7fb3f2ffb6757ff67c3027f056d
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-06-17 16:06:18 -0400

    Issue 876 Visualize Metrics from MCZbase Data;

--- metrics/js/metrics.js
+++ metrics/js/metrics.js
@@ -1,18 +1,14 @@
-function getAnnualNumbers(collection_id,targetDivId) { 
+function getAnnualNumbers(endDate,beginDate) { 
 	jQuery.ajax({
 		url: "/metrics/component/functions.cfc",
 		data : {
 			method : "getAnnualNumbers",
-			collection_id: collection_id,
 			endDate: endDate,
 			beginDate: beginDate,
 		},
 		success: function (result) {
-			$("#" + targetDivId ).html(result);
-		},
-		error: function (jqXHR, textStatus, error) {
-			handleFail(jqXHR,textStatus,error,"loading annual numbers");
+			$("#annualNumbersDiv").html(result);
 		},
 		dataType: "html"
 	});
-};
+}
