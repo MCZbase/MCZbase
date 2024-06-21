f66f1a2166ccf2101ea12370982812191d9ce155
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-06-21 13:50:03 -0400

    Issue 876 Visualize Metrics from MCZbase Data;

--- metrics/js/metrics.js
+++ metrics/js/metrics.js
@@ -1,32 +1,32 @@
-function getAnnualNums(endDate,beginDate) { 
+function getCFCFunction1(endDate,beginDate) { 
 	jQuery.ajax({
 		url: "/metrics/component/functions.cfc",
 		data : {
-			method : "getAnnualNumbers",
+			method : "getLoanNumbers",
 			endDate: endDate,
 			beginDate: beginDate,
 		},
 		success: function (result) {
-			$("#annualNumbersDiv").html(result);
+			$("#loanresult").html(result);
 		},
 		dataType: "html"
 	});
 }
 
-function callCFCFunction1() {
-
-  // Create XMLHttpRequest object
-  var xhr = new XMLHttpRequest();
-
-  // Call CFC function via AJAX
-  xhr.open('GET', '/metrics/component/functions.cfc?method=getLoanNumbers&beginDate=#beginDate#&endDate=#endDate#'); 
-  xhr.send();
-
-  // Handle response
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      document.getElementById("loanresult").innerHTML = "loan results"; 
-    }
-  }
-
-}
+//function callCFCFunction1() {
+//
+//  // Create XMLHttpRequest object
+//  var xhr = new XMLHttpRequest();
+//
+//  // Call CFC function via AJAX
+//  xhr.open('GET', '/metrics/component/functions.cfc?method=getLoanNumbers&beginDate=#beginDate#&endDate=#endDate#'); 
+//  xhr.send();
+//
+//  // Handle response
+//  xhr.onload = function() {
+//    if (xhr.status === 200) {
+//      document.getElementById("loanresult").innerHTML = "loan results"; 
+//    }
+//  }
+//
+//}
