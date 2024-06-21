8c2c63a42ee3e5c96fa0c97a41e0eb3892152eb5
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-06-21 15:12:13 -0400

    Issue 876 Visualize Metrics from MCZbase Data;

--- metrics/js/metrics.js
+++ metrics/js/metrics.js
@@ -4,7 +4,7 @@
 		var beginDate = "2022-06-30"; 
 		var endDate = "2024-06-30"; 
 		// Instantiate CFC 
-		var cfc = new ColdFusion.Component("metrics/component/functions.CFC"); 
+		var cfc = new ColdFusion.Component("metrics/component/functions"); 
 		// Call CFC method, pass variables as arguments 
 		var result = cfc.getLoanNumbers(beginDate,endDate); 
 		alert(result);
