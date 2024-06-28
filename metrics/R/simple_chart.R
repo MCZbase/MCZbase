86428943a1308ab2ddae36720d3e269ba9c7e9b8
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-06-28 11:03:12 -0400

    Issue 876 Visualize Metrics from MCZbase Data; after the javascript fix;

--- metrics/R/simple_chart.R
+++ metrics/R/simple_chart.R
@@ -1,15 +1,23 @@
 #graph 1: 
 
-
 library(ggplot2)
-
-
 
 data <- read.csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
 
 str(data)
 head(data)
 names(data)
+
+ggplot(
+  data = iris, 
+  aes(x = Sepal.Length, y = Sepal.Width, color = Species)) +
+  geom_point() +
+  labs(title = "Sepal Length vs. Sepal Width by Species",
+       x = "Sepal Length",
+       y = "Sepal Width") +
+  theme_minimal()
+
+
 
 
 
