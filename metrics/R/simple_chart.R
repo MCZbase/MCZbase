0b6d2a3c49eed3dd2d5aef20ba925a0bd8b315f5
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-06-28 11:27:56 -0400

    Issue 876 Visualize Metrics from MCZbase Data; after the javascript fix;

--- metrics/R/simple_chart.R
+++ metrics/R/simple_chart.R
@@ -1,6 +1,6 @@
 #graph 1: 
 
-#library(ggplot2)
+library(ggplot2)
 
 data <- read.csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
 
@@ -8,14 +8,23 @@ str(data)
 head(data)
 names(data)
 
-ggplot(
-  data = getStats, 
-  aes(x = citation_type, y = taxa, color = Collections)) +
-  geom_point() +
-  labs(title = "Types vs. Taxa Counts by Collections",
-       x = "Types",
-       y = "Taxa Counts") +
-  theme_minimal()
+#graph 1: transitioning through time
+#animation set up
+#library(ggplot2)
+library(png)
+#library(readr)
+#library(tidyr)
+#library(dplyr)
+
+
+simple_chart <- read_csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
+chart0 <- filter(simple_chart,COLLECTION=='Cryogenic')
+
+chart1 <- ggplot(chart0, aes(x=NUMBER_OF_TYPES_WITH_IMAGES, y=NUMBER_OF_CITATIONS, fill=TYPE_STATUS)) +
+  geom_bar(stat="identity",width = 1)+
+  coord_polar("y", start=0)
+print(chart1)
+ggsave('/var/www/html/arctos/metrics/R/graphs/chart1.png',width=6, height=4,dpi=300)
 
 
 
