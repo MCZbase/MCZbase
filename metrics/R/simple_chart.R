cd7ab61bf1297394c5844000ade703f8baeb1414
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-06-28 10:59:16 -0400

    Issue 876 Visualize Metrics from MCZbase Data; after the javascript fix;

--- metrics/R/simple_chart.R
+++ metrics/R/simple_chart.R
@@ -1,16 +1,9 @@
 #graph 1: 
-#library(gganimate)
-#library(tidyverse)
+
 library(tibble)
 library(ggplot2)
 library(dplyr)
-#library(plotly)
-library(gapminder)
-library(ggthemes)
-library(gifski)
-library(png)
-library(ggthemes)
-library(av)
+
 
 data <- read.csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
 
