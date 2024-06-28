e5f823dac0128c77b4efe5b8a1331319c91b567c
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-06-28 11:37:25 -0400

    Issue 876 Visualize Metrics from MCZbase Data; after the javascript fix;

--- metrics/R/simple_chart.R
+++ metrics/R/simple_chart.R
@@ -2,22 +2,24 @@
 
 library(ggplot2)
 
-data <- read.csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
+#data <- read.csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
 
-str(data)
-head(data)
-names(data)
+#str(data)
+#head(data)
+#names(data)
 
 #graph 1: transitioning through time
 #animation set up
 #library(ggplot2)
 library(png)
-#library(readr)
+library(readr)
 #library(tidyr)
 #library(dplyr)
 
-
+# use this one for seeing on the webpage
 simple_chart <- read_csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
+# change to this for testing in R after importing chart_data.csv
+simple_chart <- chart_data
 chart0 <- filter(simple_chart,COLLECTION=='Cryogenic')
 
 chart1 <- ggplot(chart0, aes(x=NUMBER_OF_TYPES_WITH_IMAGES, y=NUMBER_OF_CITATIONS, fill=TYPE_STATUS)) +
