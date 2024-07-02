61b1e2ee7a87c97f93ce13bd8f1220cb7e9bc14c
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-07-02 10:40:23 -0400

    Issue 876 Visualize Metrics from MCZbase Data; after the javascript fix; Chart

--- metrics/R/simple_chart.R
+++ metrics/R/simple_chart.R
@@ -1,35 +1,42 @@
-#graph 1: 
+# HOLDINGS stores reported metric counts that include those not cataloged. 
+# RECEIVEDSPECIMENS stores the sum(decode(total_parts,null,1,total_parts))
+# from flat after joining on holdings to accn transactions and grouped by collection and limited by a date range for received_date.
+# RECEIVEDCATITEMS stores count(distinct collection_object_id) receivedCatitems, joined on holdings to accn based on range for received_date
+# ENTEREDCATITEMS stores the distinct collection_object_ids from flat 
+## based on accn transactions limited by a date range for entered_date
+#NCBISPECIMENS stores all other ID with NCBI up until endDate of range
 
 library(ggplot2)
+library(ggthemes)
 library(png)
 library(readr)
 library(tidyr)
 library(dplyr)
+
   
-#data <- read.csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
-#str(data)
-#head(data)
-#names(data)
+data <- read.csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
+str(data)
+head(data)
+names(data)
 
 # use this one for seeing on the webpage
 #simple_chart <- read_csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv')
 # change to this for testing in R after importing chart_data.csv
+# simple_chart <- chart_data_mk3
 
-# First, calculate means and standard deviations
-simple_chart <- chart_data_14_ %>%
-  group_by(COLLECTION)
 
-simple_chart$log_holdings <- log10(simple_chart$HOLDINGS)
-simple_chart$log_holdings <- round(log_holdings,digits = 2)
 
-simple_chart$log_cataloged_items <- log10(simple_chart$CATALOGED_ITEMS)
-simple_chart$log_cataloged_items <- round(simple_chart$log_cataloged_items,digits = 2)
+# barplot
+# displayPlot <- ggplot(simple_chart, aes(x = factor(HOLDINGS[3]), y = RECEIVEDCATITEMS[3],z =ENTEREDCATITEMS, fill = COLLECTION[3])) +
+#                       geom_bar(stat = "identity", position = "dodge") +
+#                       xlab(expression("Entered")) +
+#                       ylab(expression("Received")) +
+#                       zlab(expression("Entered")) +
+#                       scale_fill_brewer(palette = "Greens") +
+#                       theme_few()
+# 
+# print(displayPlot)
 
-# Bar plot for mean MPG with error bars (using standard error)
-ggplot(simple_chart, aes(x = factor(COLLECTION), y = ynums)) + 
-  geom_bar(stat = "identity", fill = "skyblue") + 
-  geom_errorbar(aes(ymin = mean_cataloged - se_cataloged, ymax = mean_cataloged + se_cataloged), width = .2) +
-  theme_minimal() + 
-  labs(title = "Mean Cataloged Specimens by Collection", 
-       x = "Collection", 
-       y = "Specimens Cataloged")
+
+
+
