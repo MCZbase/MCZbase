c12e9c45ef80aa7f0af15ad2ac8f31af1d4a133c
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-07-05 08:28:13 -0400

    Issue 876 Visualize Metrics from MCZbase Data; after the javascript fix; Chart

--- metrics/R/simple_chart.R
+++ metrics/R/simple_chart.R
@@ -66,7 +66,7 @@ chart1 <- ggplot(df, aes(x="", y=df$CATALOGEDITEMS, fill=COLLECTION )) +
 chart1
 
 # !!!make sure all instances in R plots, environment, Photoshop, etc are closed before refreshing webpage.
-ggsave('/var/www/html/arctos/metrics/R/graphs/chart1.png', chart1, dpi=96)
+ggsave('/var/www/html/arctos/metrics/R/graphs/chart1.png', chart1, dpi=98)
 
 # this was about labs in chart1 to put white percentages in the pie parts
 #  geom_text(aes(label = paste0(format(round(df$HOLDINGS/sum(df$HOLDINGS)*100, 1), nsmall = 0), "")), 
