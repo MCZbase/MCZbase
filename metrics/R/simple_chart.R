0c2a7149a5489b852628c0b32ecf5779c0a70b25
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-11-13 09:35:23 -0500

    Issue 876 Visualize MCZbase Data

--- metrics/R/simple_chart.R
+++ metrics/R/simple_chart.R
@@ -6,7 +6,7 @@ library(png)
 library(readr)
 
 # use this one when testing is finished and want to go "live"
-df <- read.csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv', show_col_types = FALSE)
+#df <- read_csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv', show_col_types = FALSE)
 
 # use to check for loading errors  
 # str(data)
@@ -14,10 +14,10 @@ df <- read.csv('/var/www/html/arctos/metrics/datafiles/chart_data.csv', show_col
 # names(data)
 
 #local load for testing
-#df <- read_csv("C:/Users/mih744/Downloads/chart_data.csv")
-
+df <- read_csv("C:/Users/mih744/Downloads/chart_data.csv")
+df <- chart_data
 # make calculations based on collection grouping
-#df %>% group_by(COLLECTIONS)
+df %>% group_by(COLLECTIONS)
 
 # filter out Herp Obs row (only 47 rows -- outlier)
 filter <- !(df$CATALOGEDITEMS == 47)
@@ -34,7 +34,7 @@ chart1 <- ggplot(df, aes(x="", y=CATALOGEDITEMS, fill=COLLECTION )) +
        caption = "Source: Cataloged Items from MCZbase") +
  theme_void()
 # uncomment and use chart or print(chart1) during testing
-#chart1
+chart1
 
 # !!!make sure all instances in R plots, environment, Photoshop, etc are closed before refreshing webpage.
 ggsave('/var/www/html/arctos/metrics/R/graphs/chart1.png', chart1, width=7, height=5, units="in", dpi=96)
