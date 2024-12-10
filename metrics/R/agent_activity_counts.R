910551a2f75ac1212c747de180b7d68c697ce4b8
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-12-10 17:04:02 -0500

    Issue 876 Visualize MCZbase Data

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -3,14 +3,15 @@ library(readr)
 library(ggplot2)
 library(dplyr)
 library(patchwork)
-agents_roles <- read.csv("C:/Users/mih744/RedesignMCZbase/metrics/datafiles/Agent_roles_last.csv")
+agents_roles <- read.csv("C:/Users/mih744/RedesignMCZbase/metrics/datafiles/agent_activity_counts.csv")
 # removes NAs
 agents_data <- agents_roles[complete.cases(agents_roles), ]
 #First replace.
 agents_data$AGENT_ID <- as.numeric(as.character(agents_data$AGENT_ID))
 agents_data$AGENT_ID[is.na(agents_data$AGENT_ID)] <- 0  # Replace NAs with 0 or another appropriate value 
 # Then remove
-agents_data <-  agents_data[agents_data$AGENT_ID != "0",]  
+agents_data <-  agents_data[agents_data$AGENT_ID != "0",]
+
 # Unite AgentID and AgentName to create a unique AgentInfo combination
 agents_data_name <- agents_data %>%
   unite("AgentInfo", AGENT_ID, AGENT_NAME, sep = " - ")
@@ -26,6 +27,8 @@ total_counts <- agents_data_sorted %>%
   group_by(AgentInfo) %>%
   summarize(TotalCount = sum(COUNT)) %>%
   ungroup()
+
+
 ###############code finds outliers
 # any(is.na(agents_data_role$AgentInfo))  # Check for any NA values
 
@@ -33,7 +36,7 @@ total_counts <- total_counts %>%
   mutate(TotalCount = as.numeric(TotalCount))
 
 total_counts_filtered <- total_counts %>%
-  filter(TotalCount >= 0)
+  filter(TotalCount > 200)
 # Create the RoleLabel by combining RoleNumber and Role
 # Assign RoleNumbers and automate factor conversion
 agents_data_sorted <- agents_data_sorted %>%
@@ -82,12 +85,12 @@ outliers$AgentInfo <- factor(outliers$AgentInfo, levels = total_counts_sorted$Ag
 # The display is below: Define a custom palette corresponding to the roles
 custom_palette <- c("#E69F00","#4b0082","#006400","#03839c","#2f4f4f","#394df2",
                     "#483D8B","#4682b4","#8b0000","#8B008B","#8B3a3a","#a0522d",
-                    "#708090","#556B2F","#b53b56","#106a93","#6A5ACD","#cd4b19",
-                    "#4daf4a","#ff7f00","#665433","#096d28","#FF4500","#a892f5",
-                    "#f00000","#334445","#a8786f","#5a5a5a","#556B2F","#0072B2",
-                    "#657843","#a65628","#006400","#f75147","#c42e24","#56B4E9",
-                    "#234b34","#377eb8","#432666","#e22345","#d24678","#0073e6",
-                    "#984ea3","#b51963","#5928ed","#00008b","#2e8b57"
+                    "#708090","#b53b56","#106a93","#6A5ACD","#cd4b19","#4daf4a",
+                    "#ff7f00","#665433","#096d28","#FF4500","#a892f5","#f00000",
+                    "#334445","#a8786f","#5a5a5a","#0072B2","#657843","#a65628",
+                    "#006400","#f75147","#c42e24","#56B4E9","#234b34","#432666",
+                    "#e22345","#d24678","#0073e6","#984ea3","#b51963","#556B2F",
+                    "#5928ed","#00008b","#2e8b57"
 )
 
 # Use RoleLabel for legend labels, which should be unique
