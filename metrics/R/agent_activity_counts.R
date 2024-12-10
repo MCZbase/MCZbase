8e95ecad5165025a343354e97a1260be9084a3cf
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-12-10 17:07:38 -0500

    Issue 876 Visualize MCZbase Data

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -3,7 +3,8 @@ library(readr)
 library(ggplot2)
 library(dplyr)
 library(patchwork)
-agents_roles <- read.csv("C:/Users/mih744/RedesignMCZbase/metrics/datafiles/agent_activity_counts.csv")
+library(png)
+agents_roles <- read_csv('/var/www/html/arctos/metrics/datafiles//agent_activity_counts.csv', show_col_types = FALSE)
 # removes NAs
 agents_data <- agents_roles[complete.cases(agents_roles), ]
 #First replace.
