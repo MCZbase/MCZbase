f5c1370399dec96e92fc36cf3fdb74177d0b3508
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-12-17 14:14:47 -0500

    Issue 876 Visualize MCZbase Data

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -1,11 +1,9 @@
 # Read in the libraries
 library(readr)
 library(ggplot2)
-library(dplyr)
+library(tidyverse)
 library(patchwork)
 library(png)
-library(stringr)
-
 agents_roles <- read_csv('C:/Users/mih744/RedesignMCZbase/metrics/datafiles/agent_activity_counts.csv', show_col_types=FALSE)
 #agents_roles <- read_csv('/var/www/html/arctos/metrics/datafiles/agent_activity_counts.csv', show_col_types = FALSE)
 # removes NAs
@@ -16,12 +14,9 @@ agents_data$AGENT_ID[is.na(agents_data$AGENT_ID)] <- 0  # Replace NAs with 0 or 
 # Then remove
 agents_data <-  agents_data[agents_data$AGENT_ID != "0",]
 
-# Example vector of employee names
-agents_data$truncated_names <- str_trunc(agents_data$AGENT_NAME, width=15, side="right")
-
 # Unite AgentID and AgentName to create a unique AgentInfo combination
 agents_data_name <- agents_data %>%
-  unite("AgentInfo", AGENT_ID, truncated_names, sep = " - ")
+  unite("AgentInfo", AGENT_ID, AGENT_NAME, sep = " - ")
 
 agents_data_role <- agents_data_name %>%
   unite("Role", TABLE_NAME, COLUMN_NAME, sep = ".")
@@ -35,6 +30,7 @@ total_counts <- agents_data_sorted %>%
   summarize(TotalCount = sum(COUNT)) %>%
   ungroup()
 
+
 ###############code finds outliers
 # any(is.na(agents_data_role$AgentInfo))  # Check for any NA values
 
@@ -42,8 +38,7 @@ total_counts <- total_counts %>%
   mutate(TotalCount = as.numeric(TotalCount))
 
 total_counts_filtered <- total_counts %>%
-  filter(TotalCount > 6000)
-
+  filter(TotalCount > 200)
 # Create the RoleLabel by combining RoleNumber and Role
 # Assign RoleNumbers and automate factor conversion
 agents_data_sorted <- agents_data_sorted %>%
@@ -70,85 +65,74 @@ total_counts_sorted <- total_counts_filtered %>%
 
 # Assign and connect the role numbers to the roles based on the order in the datasheet
 # find a way to order them by the agents and not by position
-role_order <- c(agents_data_sorted$Role[1:13])
+role_order <- c(agents_data_sorted$Role[1:26])
 role_numbers <- setNames(1:length(role_order), role_order)
 
 # Separate main data and outliers based on identified agents
 main_data <- agents_data_sorted %>%
   filter(!AgentInfo %in% outliers_agents)
 
-#this doesn't seem to be doing anything. want to remove NAs
-main_data_filtered <- main_data[!is.na(main_data$AgentInfo),]
-
 outliers <- agents_data_sorted %>%
   filter(AgentInfo %in% outliers_agents)
 
 # PER PERSON ORDER: Order stacks within each person by their count in the main_data
-main_data_filtered <- main_data_filtered %>%
+main_data <- main_data %>%
   arrange(AgentInfo, desc(AdjustedCount))
 outliers <- outliers %>%
   arrange(AgentInfo, desc(AdjustedCount))
 
-main_data_filtered$AgentInfo <- factor(main_data_filtered$AgentInfo, levels = total_counts_sorted$AgentInfo)
-
-main_data_filtered <- na.omit(main_data_filtered)
-
+main_data$AgentInfo <- factor(main_data$AgentInfo, levels = total_counts_sorted$AgentInfo)
 outliers$AgentInfo <- factor(outliers$AgentInfo, levels = total_counts_sorted$AgentInfo)
 
-
 # The display is below: Define a custom palette corresponding to the roles
-custom_palette <- c("#E69F00","#8B008B","#4b0082","#03839c","#2f4f4f",
-                    "#394df2","#e22345","#483D8B","#2e8b57","#4682b4",
-                    "#a0522d","#708090","#006400","#b53b56","#56B4E9",
-                    "#106a93","#6A5ACD","#cd4b19","#4daf4a","#ff7f00",
-                    "#665433","#096d28","#FF4500","#a892f5","#f00000",
-                    "#334445","#a8786f","#5a5a5a","#0072B2","#657843",
-                    "#d24678","#a65628","#006400","#f75147","#c42e24",
-                    "#234b34","#432666","#0073e6","#984ea3","#b51963",
-                    "#8b0000","#556B2F","#8B3a3a","#5928ed","#00008b"
+custom_palette <- c("#E69F00","#4b0082","#006400","#03839c","#2f4f4f","#394df2",
+                    "#483D8B","#4682b4","#8b0000","#8B008B","#8B3a3a","#a0522d",
+                    "#708090","#b53b56","#106a93","#6A5ACD","#cd4b19","#4daf4a",
+                    "#ff7f00","#665433","#096d28","#FF4500","#a892f5","#f00000",
+                    "#334445","#a8786f","#5a5a5a","#0072B2","#657843","#a65628",
+                    "#006400","#f75147","#c42e24","#56B4E9","#234b34","#432666",
+                    "#e22345","#d24678","#0073e6","#984ea3","#b51963","#556B2F",
+                    "#5928ed","#00008b","#2e8b57"
 )
 
 # Use RoleLabel for legend labels, which should be unique
 legend_labels <- unique(agents_data_sorted$RoleLabel)
-df_cleaned <- anti_join(main_data_filtered, agents_data_sorted, by = "AgentInfo")
 
 # Main plot for standard range, exclude full stacks that are moved to outliers
-main_plot <- ggplot(main_data_filtered, aes(x = AgentInfo, y = AdjustedCount, fill=Role)) +
+main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role)) +
   geom_bar(stat = "identity", position = "stack") +
-  geom_text(aes(label = ifelse(AdjustedCount > 2000, paste0(as.integer(factor(Role)), ""), "")),  # Conditionally show label
+  geom_text(aes(label = ifelse(AdjustedCount > 5000, paste0(as.integer(factor(Role)), ""), "")),  # Conditionally show label
             position = position_stack(vjust = 0.5),
-            size = 3.75, color = "white", fontface = "bold") +
-  labs(title = "Activity Counts by Role for Each Agent", x = "Agent Info (Agent ID + Login Name)",
-       y = "Activity Counts (<= 100,000)", fill = "Roles") +
+            size = 2.5, color = "white", fontface = "bold") +
+  labs(title = "Counts by Role and Agent", x = "Agent Info",
+       y = "COUNT (<= 100,000)", fill = "Role Legend") +
   scale_color_manual(values = custom_palette) +
   scale_fill_manual(values = c(custom_palette), labels = unique(agents_data_sorted$RoleLabel)) +
   scale_y_continuous(labels = scales::comma, expand = c(0.02, 0.02)) +
   theme_minimal() +
-  theme(axis.text.x = element_text(size=10,angle =50, hjust = 1)) 
+  theme(axis.text.x = element_text(size=8,angle =50, hjust = 1)) 
 
 
 # Outliers plot, now includes whole removed stacks
 outliers_plot <- ggplot(outliers, aes(x = AgentInfo, y = AdjustedCount, fill = Role)) +
   geom_bar(stat = "identity", position = "stack") + 
-  geom_text(aes(label = ifelse(AdjustedCount > 5000, paste0(as.integer(factor(Role)), ""), "")), 
-            size = 3.75, color = "white",position=position_stack(vjust=0.5)) +
-  scale_fill_manual(values = custom_palette, labels = unique(main_data_filtered$RoleLabel),guide="none") +
-  scale_y_continuous(labels = scales::comma, expand = c(0.02, 0.02)) + 
+  geom_text(aes(label = ifelse(AdjustedCount > 0, paste0(as.integer(factor(Role)), ""), "")), 
+            size = 2.5, color = "white",position=position_stack(vjust=0.5)) +
+  scale_fill_manual(values = custom_palette, labels = unique(agents_data_sorted$RoleLabel),guide="none") +
+  scale_y_continuous(labels = scales::comma) + 
   theme_minimal() +
-  labs(title = "Outlier Counts", x = NULL, y = "Activity Count (High: > 100,000)", fill = NULL) +
-  theme(axis.text.x = element_text(size=10,angle =50, hjust = 1)) 
+  labs(title = "Outlier Counts", x = NULL, y = "COUNT (> 100000)", fill = NULL) +
+  theme(axis.text.x = element_text(size=8,angle =50, hjust = 1)) 
 
 # Combine the plots using patchwork, place outliers to the left and merge legends
 combined_plot <- main_plot + outliers_plot +
   plot_layout(guides = 'collect', widths = c(12,1)) & 
-  theme(legend.position = 'bottom', legend.box="vertical", legend.key.size = unit(0.45, "cm"),
-        legend.key.width = unit(.45, "cm"),legend.text = element_text(size = 9),
-        legend.box.background = element_rect(color = "black"), legend.spacing = unit(5, "cm"),guides(fill = guide_legend(ncol = 1)))
+  theme(legend.position = 'bottom', legend.box="vertical", legend.key.size = unit(0.3, "cm"),
+        legend.key.width = unit(.23, "cm"),legend.text = element_text(size = 8),
+        legend.spacing = unit(5, "cm"),guides(fill = guide_legend(ncol = 1)))
 
 # Display the combined plot
 print(combined_plot)
 
 # !!!make sure all instances in R plots, environment, Photoshop, etc are closed before refreshing webpage.
 ggsave('/var/www/html/arctos/metrics/R/agent_role_chart.png', combined_plot, width=1, height=11, units="in", dpi=96)
-
-
