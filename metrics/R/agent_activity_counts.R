cdc47585e30a827485dd5d0869e10b6fe6835330
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-12-18 16:18:32 -0500

    Issue 876 Visualize MCZbase Data

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -120,11 +120,11 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
   scale_fill_manual(values = c(palette), labels = unique(agents_data_sorted$RoleLabel)) +
   scale_y_continuous(labels = scales::comma, expand = c(0.02, 0.02)) +
   theme_minimal() +
-  theme(plot.title = element_text(size=unit(12,"pt"), face="bold"), 
-        axis.text.x = element_text(size=unit(10,"pt"),angle =50, hjust = 1),
-        axis.text.y = element_text(size=unit(10,"pt")),
-        axis.title.x = element_text(size=unit(12,"pt")),
-        axis.title.y = element_text(size=unit(12,"pt"))
+  theme(plot.title = element_text(size=unit(10,"pt"), face="bold"), 
+        axis.text.x = element_text(size=unit(6,"pt"),angle =50, hjust = 1),
+        axis.text.y = element_text(size=unit(6,"pt")),
+        axis.title.x = element_text(size=unit(8,"pt")),
+        axis.title.y = element_text(size=unit(8,"pt"))
   )  
 
 
@@ -150,10 +150,11 @@ outliers_plot <- ggplot(outliers, aes(x = AgentInfo, y = AdjustedCount, fill = R
        y = "COUNT (> 100000)", 
        fill = NULL
        ) +
-  theme(plot.title = element_text(size=unit(13,"pt"), face="bold"), 
-        axis.text.x = element_text(size=unit(10,"pt"),angle =50, hjust = 1),
-        axis.text.y = element_text(size=unit(10,"pt")),
-        axis.title.y = element_text(size=unit(12,"pt"))
+  theme(plot.title = element_text(size=unit(10,"pt"), face="bold"), 
+        axis.text.x = element_text(size=unit(6,"pt"),angle =50, hjust = 1),
+        axis.text.y = element_text(size=unit(6,"pt")),
+        axis.title.y = element_text(size=unit(8,"pt")),
+        axis.title.x = element_text(size=unit(8,"pt"))
         ) 
 
 # Combine the plots using patchwork, place outliers to the left and merge legends
@@ -163,9 +164,9 @@ combined_plot <- main_plot + outliers_plot +
     legend.position = "bottom",               # Place legend at the bottom
     legend.direction = "horizontal",          # Arrange legend items in a row
     legend.box = "horizontal",            # Ensure across-the-page spread
-    legend.key.size = unit(0.5, "cm"),        # Adjust the size of the legend key
-    legend.key.height = unit(0.4, "cm"),      # Optionally adjust height separately
-    legend.key.width = unit(0.4, "cm")        # Optionally adjust width separately
+    legend.key.size = unit(0.1, "cm"),        # Adjust the size of the legend key
+    legend.key.height = unit(0.1, "cm"),      # Optionally adjust height separately
+    legend.key.width = unit(0.1, "cm")        # Optionally adjust width separately
     
   ) 
 # Display the combined plot
