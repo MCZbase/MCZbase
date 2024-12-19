7e4ed49dfccce7c9395432a82afb2165b7209c7f
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-12-19 16:44:07 -0500

    Issue 876 Visualize MCZbase Data

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -163,19 +163,20 @@ outliers_plot <- ggplot(outliers, aes(x = AgentInfo, y = AdjustedCount, fill = R
 combined_plot <- main_plot + outliers_plot +
   plot_layout(guides = 'collect', widths = c(19.5, 1.5)) & 
   theme(
-    legend.position.inside = c(1, 1), # Adjust the coords to fit your specific data
+    
     legend.direction = "vertical",   # Typically more space-efficient when inside plots
     legend.box = "vertical",
     legend.background = element_rect(fill=alpha('white', 0.0)), # Make the legend background transparent
     legend.key.size = unit(0.5, "lines"),
     legend.box.margin = margin(0, 0, 0, 0), # Tighten the box margin if needed
     legend.text = element_text(size=3.2),
-    legend.title = element_text(size=3.2),
+    legend.title = element_text(size=4.5),
     legend.spacing.x = unit(0.05, "cm"),
     legend.spacing.y = unit(0.05, "cm"),
     plot.margin = margin(5,5,5,5),
     legend.margin = margin(3, 3, 6, 3), # Reduce margin around the legend
-    legend.box.spacing = unit(0.02, "cm") # Adjust spacing between legend box and plot
+    legend.box.spacing = unit(0.02, "cm"), # Adjust spacing between legend box and plot
+    legend.position.inside = c(1, 1), # Adjust the coords to fit your specific data
   ) 
 # Display the combined plot
 #print(combined_plot)
