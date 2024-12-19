c7aadb5dd07d29aef8c0600117d9e1ae210aea00
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2024-12-19 17:08:11 -0500

    Issue 876 Visualize MCZbase Data

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -133,7 +133,7 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
   )+
  basic + theme(
     legend.direction = "vertical",   # Typically more space-efficient when inside plots
-    legend.box = "vertical",
+   # legend.box = "vertical",
     legend.background = element_rect(fill=alpha('white', 0.0)), # Make the legend background transparent
     legend.key.size = unit(0.5, "lines"),
     legend.box.margin = margin(0, 0, 0, 0), # Tighten the box margin if needed
@@ -141,14 +141,14 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
     legend.title = element_text(size=4.5),
     legend.spacing.x = unit(0.05, "cm"),
     legend.spacing.y = unit(0.05, "cm"),
-    plot.margin = margin(5,5,5,5),
+    plot.margin = margin(5,0,5,5),
    # legend.margin = margin(3, 3, 6, 3), # Reduce margin around the legend
     legend.box.spacing = unit(0.02, "cm"), # Adjust spacing between legend box and plot
    # legend.position.inside = c(0.8, 0.2), # Adjust the coords to fit your specific data
     legend.position = c(.95, .95),
     legend.justification = c("right", "top"),
     legend.box.just = "right",
-    legend.margin = margin(6, 6, 6, 6)
+    legend.margin = margin(6, 6, 6, 2)
   )
   ) 
 
