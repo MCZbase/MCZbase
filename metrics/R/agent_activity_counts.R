6bdb0b4a358c0d1c251f2b611bbc4fa639d7880e
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2025-01-02 15:26:43 -0500

    Issue 876 Visualize MCZbase Data

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -163,7 +163,7 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
         legend.key.size = unit(0.355, "lines"),
         legend.box.margin = margin(0.05, 0.05, 0.05, 0.05), # Tighten the box margin if needed
         legend.text = element_text(size=3.4),
-        legend.title = element_text(size=4.5),
+        legend.title = element_text(margin = margin(0, 0, -10, 0), size=4.5), 
         legend.spacing.x = unit(0.02, "cm"),
         legend.spacing.y = unit(0.05, "cm"),
         legend.justification = c("right", "top"),
