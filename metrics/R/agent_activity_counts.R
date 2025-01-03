1333d5f6a63ad400418ca06ec019fc299e4b67cb
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2025-01-03 15:32:41 -0500

    Issue 876 Visualize MCZbase Data; One agents Activity

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -145,7 +145,7 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
                 position = position_stack(vjust = 0.5),
                 size = 1, color = "white"
                 ) +
-  labs(title = "Counts by Role and Agent", subtitle = "(> 3500 shown)",
+  labs(title = "Counts by Role and Agent <span style='font-size: 14px;'>(> 3500 shown)</span>",
        x = "Agent Info",y = "COUNT (<= 100,000)") +
   scale_color_manual(values=cpalette,labels=unique(agents_data_sorted$simplified)) +
   scale_fill_manual(values=cpalette,labels=agents_data_sorted$simplified) +
@@ -153,7 +153,6 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
   theme_minimal() +
   theme(plot.title = element_text(size=unit(7,"pt"), face="bold"),
         plot.margin = margin(t=1,r=1,b=1,l=1),
-        plot.subtitle = element_markdown(size = 3),
         axis.text.x = element_text(margin=margin(t=0.025), size=unit(3.5,"pt"), angle =50, hjust = 1),
         axis.text.y = element_text(margin=margin(t=0.025), size=unit(3.5,"pt")),
         axis.title.x = element_text(size=unit(5,"pt")),
@@ -163,7 +162,7 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
         legend.background = element_rect(fill=alpha('white', 0.0)), # Make the legend background transparent
         legend.key.size = unit(0.355, "lines"),
         legend.box.margin = margin(0.05, 0.05, 0.05, 0.05), # Tighten the box margin if needed
-        legend.text = element_text(margin=margin(0, 0, 0, 0.02), size=3.5),
+        legend.text = element_text(margin=margin(0, 0, 0, 0.02), size=),
         legend.spacing.x = unit(0.02, "cm"),
         legend.spacing.y = unit(0.05, "cm"),
         legend.justification = c("right", "top"),
