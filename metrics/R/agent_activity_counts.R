9a17def45da5ebb0145477439336b4102f16cf58
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2025-01-03 15:20:26 -0500

    Issue 876 Visualize MCZbase Data; One agents Activity

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -8,6 +8,7 @@ library(dplyr)
 library(patchwork)
 library(svglite)
 library(stringr)
+library(ggtext)
 ## change to locally saved csv for running the code while developing
 #agents_roles <- read_csv('C:/Users/mih744/RedesignMCZbase/metrics/datafiles/agent_activity_counts.csv', show_col_types=FALSE)
 agents_roles <- read_csv('/var/www/html/arctos/metrics/datafiles/agent_activity_counts.csv', show_col_types = FALSE)
@@ -144,7 +145,7 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
                 position = position_stack(vjust = 0.5),
                 size = 1, color = "white"
                 ) +
-  labs(title = "Counts by Role and Agent (> 3500 shown)",x = "Agent Info",y = "COUNT (<= 100,000)") +
+  labs(title = "Counts by Role and Agent <span style = 'font-size: 12px'>(> 3500 shown)</span>",x = "Agent Info",y = "COUNT (<= 100,000)") +
   scale_color_manual(values=cpalette,labels=unique(agents_data_sorted$simplified)) +
   scale_fill_manual(values=cpalette,labels=agents_data_sorted$simplified) +
   scale_y_continuous(labels = scales::comma, expand=c(0.02, 0.02)) +  # removed this after comma: ", expand = c(0.02, 0.02)" makes space between labels and text smaller
