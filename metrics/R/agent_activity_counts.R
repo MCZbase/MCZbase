0f6c00ae179b1a77e38b3a7a1738e5ee60a38899
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2025-04-10 13:07:30 -0400

    Issue 983 Agent Metrics. update R file

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -153,7 +153,7 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
   theme_minimal() +
   theme(plot.title = element_text(size=unit(7,"pt"), face="bold"),
         plot.margin = margin(t=1,r=1,b=1,l=1),
-        axis.text.x = element_text(margin=margin(t=-1,b=-1), size=unit(0.5,"pt"), color='white', angle =0, hjust = 0),
+        axis.text.x = element_text(margin=margin(t=-1,b=-1), size=unit(0.5,"pt"), color='black', angle =0, hjust = 0),
         axis.text.y = element_text(margin=margin(t=0.025), size=unit(3.5,"pt")),
         axis.title.x = element_text(size=unit(5,"pt")),
         axis.title.y = element_text(size=unit(5,"pt")), 
