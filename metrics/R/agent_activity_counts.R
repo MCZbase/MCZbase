26aace43e989fffb89bc255f930b11dfb5395074
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2025-04-14 16:06:44 -0400

    Issue 983 Agent Metrics. update R file

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -194,7 +194,7 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
         legend.direction = "vertical",   # Typically more space-efficient when inside plots
         legend.box = "vertical",
         legend.background = element_rect(fill=alpha('white', 0.0)), # Make the legend background transparent
-        legend.key.size = unit(0.5, "lines"),
+        legend.key.size = unit(0.3, "lines"),
         legend.box.margin = margin(0.05, 0.05, 0.05, 0.05), # Tighten the box margin if needed
         legend.text = element_text(size=rel(0.4)),
         legend.spacing.x = unit(0.02, "cm"),
