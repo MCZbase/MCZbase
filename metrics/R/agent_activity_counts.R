fe6974a40882165c902d3d297ccc927142b11cdc
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2025-04-14 15:53:17 -0400

    Issue 983 Agent Metrics. update R file

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -185,7 +185,7 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
   scale_fill_manual(values=cpalette,labels=agents_data_sorted$simplified) +
   scale_y_continuous(labels = scales::comma, expand=c(0.02, 0.02)) +  # removed this after comma: ", expand = c(0.02, 0.02)" makes space between labels and text smaller
   theme_minimal(base_size = 12) +
-  theme(plot.title = element_text(size=rel(0.6), face="bold"),
+  theme(plot.title = element_text(size=rel(0.7), face="bold"),
         plot.margin = margin(t=1,r=1,b=0,l=10),
         axis.text.x = element_text(margin=margin(t=-1,b=-1), size=rel(0.5), color='white', angle =0, hjust = 0),
         axis.text.y = element_text(margin=margin(t=0.025), size=rel(0.5)),
@@ -196,12 +196,12 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
         legend.background = element_rect(fill=alpha('white', 0.0)), # Make the legend background transparent
         legend.key.size = unit(0.5, "lines"),
         legend.box.margin = margin(0.05, 0.05, 0.05, 0.05), # Tighten the box margin if needed
-        legend.text = element_text(size=rel(0.5)),
+        legend.text = element_text(size=rel(0.4)),
         legend.spacing.x = unit(0.02, "cm"),
         legend.spacing.y = unit(0.05, "cm"),
         legend.justification = c("right", "top"),
         legend.box.just = "right",
-        legend.title = element_text(size=0.5, hjust=0.5), 
+        legend.title = element_text(size=0.8, hjust=0.5), 
         legend.margin = margin(2, 2, 2, 2)
   )
 
