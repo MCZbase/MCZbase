2f69f432af4341f25bba5c1e4e850f8e9eeb5fac
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2025-04-15 18:52:35 -0400

    Issue 983 Agent Metrics. update R file; font

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -180,18 +180,18 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
     scale_fill_manual(name="Type of Action",values=cpalette,labels=agents_data_sorted$simplified) +
     scale_y_continuous(labels = scales::comma, expand=c(0.02, 0.02)) +  # removed this after comma: ", expand = c(0.02, 0.02)" makes space between labels and text smaller
     theme_minimal(base_size = 12) +
-    theme(plot.title = element_text(size=rel(0.4), family="Roboto Bold"),
+    theme(plot.title = element_text(size=rel(0.4), family="Roboto Black"),
         plot.margin = margin(t=0,r=1,b=0,l=10),
         axis.text.x = element_text(margin=margin(t=0,b=0), size=rel(0.36), color='white', angle =0, hjust = 0),
         axis.text.y = element_text(margin=margin(t=0.25), size=rel(0.36), family="Roboto Regular"),
-        axis.title.x = element_text(margin=margin(t=0,b=0), size=rel(0.36),family="Roboto Bold"),
-        axis.title.y = element_text(size=rel(0.36),family="Roboto Bold"), 
+        axis.title.x = element_text(margin=margin(t=0,b=0), size=rel(0.36),family="Roboto Black"),
+        axis.title.y = element_text(size=rel(0.36),family="Roboto Black"), 
         legend.direction = "vertical",   # Typically more space-efficient when inside plots
         legend.box = "vertical",
         legend.background = element_rect(fill=alpha('white', 0.0)), # Make the legend background transparent
         legend.key.size = unit(0.33, "lines"),
         legend.box.margin = margin(t=1, r=1, b=1,l=1), # Tighten the box margin if needed
-        legend.text = element_text(margin=margin(l=0.5),size=rel(0.32),hjust=0),
+        legend.text = element_text(margin=margin(l=0.5),size=rel(0.32),hjust=0,family="Roboto Regular"),
         legend.spacing.x = unit(0.02, "cm"),
         legend.spacing.y = unit(0.02, "cm"),
         legend.justification = c("right", "top"),
@@ -218,12 +218,12 @@ outliers_plot <- ggplot(outliers, aes(x = AgentInfo, y = AdjustedCount, fill = R
               y = "Count of actions (> 100,000)", 
               fill = NULL
               ) +
-      theme(plot.title = element_text(size=rel(0.4),family="Roboto Bold"), 
+      theme(plot.title = element_text(size=rel(0.4),family="Roboto Black"), 
             plot.margin = margin(t=0,r=1,b=0,l=3),
             axis.text.x = element_text(margin=margin(t=0,b=0), size=rel(0.36), color='white', angle =0, hjust = 0),
             axis.text.y = element_text(margin=margin(t=0.25), size=rel(0.36),family="Roboto Regular"),
-            axis.title.x = element_text(margin=margin(t=0,b=0), size=rel(0.36),family="Roboto Bold"),
-            axis.title.y = element_text(size=rel(0.36), family="Roboto Bold")
+            axis.title.x = element_text(margin=margin(t=0,b=0), size=rel(0.36),family="Roboto Black"),
+            axis.title.y = element_text(size=rel(0.36), family="Roboto Black")
             ) 
 
 ## Combine the plots using patchwork, place outliers to the left and merge legends
