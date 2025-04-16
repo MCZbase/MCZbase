92e39b26cd6642c42033dd9c4cff47cf8e12df22
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2025-04-16 09:59:02 -0400

    Issue 983 Agent Metrics. update R file; outlier plot /legend margins

--- metrics/R/agent_activity_counts.R
+++ metrics/R/agent_activity_counts.R
@@ -190,14 +190,14 @@ main_plot <- ggplot(main_data, aes(x = AgentInfo, y = AdjustedCount, fill=Role))
         legend.box = "vertical",
         legend.background = element_rect(fill=alpha('white', 0.0)), # Make the legend background transparent
         legend.key.size = unit(0.33, "lines"),
-        legend.box.margin = margin(1,1,1,0,unit="pt"), # Tighten the box margin if needed
+        legend.box.margin = margin(3,3, 3, 3, unit = "pt"), # Tighten the box margin if needed
         legend.text = element_text(margin=margin(l=0.5),size=rel(0.32),hjust=0,family="Roboto Regular"),
         legend.spacing.x = unit(0.02, "cm"),
         legend.spacing.y = unit(0.02, "cm"),
         legend.justification = c("right", "top"),
         legend.box.just = "left",
         legend.title = element_text(margin=margin(b=2),size=rel(0.36), hjust=0.5, family="Roboto Bold"), 
-        legend.margin = margin(2, 2, 2, 0, unit="pt")#inside of box
+        legend.margin = element_text(margin=margin(t=3,r=2,b=3,l=2)) #inside of box
     )
 
 ## Outliers plot, now includes whole removed stacks
@@ -219,7 +219,7 @@ outliers_plot <- ggplot(outliers, aes(x = AgentInfo, y = AdjustedCount, fill = R
               fill = NULL
               ) +
       theme(plot.title = element_text(size=rel(0.4),family="Roboto Black"), 
-            plot.margin = margin(0,0,0,0,unit="pt"),
+            plot.margin = margin(0,0,0,3,unit="pt"),
             axis.text.x = element_text(margin=margin(t=0,b=0), size=rel(0.36), color='white', angle =0, hjust = 0),
             axis.text.y = element_text(margin=margin(t=0.25), size=rel(0.36),family="Roboto Regular"),
             axis.title.x = element_text(margin=margin(t=0,b=0), size=rel(0.36),family="Roboto Black"),
