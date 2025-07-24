56a367538238e91392611fb99201d8ae511fd882
chicoreus <mole@morris.net>
2025-07-24 13:05:40 -0400

    ISSUE: Redmine 985 PURPOSE: Move proof of concept wiki page loader to production. DESCRIPTION: Moving javascript into test branch.

--- shared/js/shared-scripts.js
+++ shared/js/shared-scripts.js
@@ -2,6 +2,110 @@
  * Place scripts that should be available on all web pages for all users here.
 */
 
+/**** Functions to load wiki content and display it in a target div within an MCZbase page, including support for a wikiDrawer drawer container ******/
+
+/**
+ * Show a wiki article in a target div, with options for showing images and specifying a target div for content.
+ * @param page the name of the wiki page to load.
+ * @param showImages boolean indicating whether to show images in the article, false to exclude images, true to include them.
+ * @param targetDiv the id of the div to place the content into without a leading # selector.
+ * @param titleTargetDiv the id of the div to place the title into without a leading # selector.
+ * @param openFunction optional, a function to call when the wiki content is successfully loaded, e.g. to open a drawer.
+ * @param closeFunction optional, a function to call when the wiki content fails to load, e.g. to close a drawer.
+ * @param titleLink boolean indicating whether to create a link to the wiki page in the title div, true to create a link, false to just show the title.
+ * @param section optional, the section number to load from the wiki page, default 0 for the entire wiki article.
+ */
+function showWiki(page, showImages, targetDiv, titleTargetDiv, openFunction, closeFunction, titleLink, section=null) {
+	$('#'+targetDiv).html('Loading...');
+	if (titleLink) {
+		$('#'+titleTargetDiv).html('Wiki Article: <a href="https://code.mcz.harvard.edu/wiki/index.php?title=' + page + '" target="_blank">' + page + '</a>');
+	} else {
+		$('#'+titleTargetDiv).html('Wiki Article: ' + page);
+	}
+	$.ajax({
+		url: '/shared/component/functions.cfc?method=getWikiArticle',
+		data: {
+			page: page,
+			showImages: showImages,
+			section: section,
+			returnFormat: 'json'
+		},
+		dataType: 'json',
+		success: function(response) {
+			var html = response.result || response.RESULT || "<div>Section not found.</div>";
+			if (typeof openFunction === 'function') {
+				openFunction();
+			}
+			if (typeof onSuccess === 'function') {
+				options.onSuccess(html);
+			} else {
+				$('#'+targetDiv).html(html);
+				processWikiContent($('#'+targetDiv));
+			}
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			if (typeof closeFunction === 'function') {
+				closeFunction();
+			}
+			handleFail(jqXHR, textStatus, errorThrown, "loading wiki content for page: " + page);
+		}
+	});
+}
+// Shared wiki drawer open/close functions, assume wiki drawer is a div with id wikiDrawer, and
+// that there are show-wiki and hide-wiki buttons to toggle with the drawer.
+function openWikiDrawer() {
+	$('#wikiDrawer').addClass('open');
+	$('#content').addClass('pushed');
+	$("#show-wiki").hide();
+	$("#hide-wiki").show();
+}
+function closeWikiDrawer() {
+	$('#wikiDrawer').removeClass('open');
+	$('#content').removeClass('pushed');
+	$("#show-wiki").show();
+	$("#hide-wiki").hide();
+}
+
+// Shared process/cleanup wiki content
+function processWikiContent($container) {
+	$container.find('.mw-editsection').remove(); // remove edit controls
+	$container.find('#toc').remove(); // remove table of contents
+
+	$container.find('a').filter(function() {
+		return $(this).text().trim().toLowerCase() === "edit";
+	}).remove(); // remove edit links within the content.
+
+	$container.html($container.html().replace(/edit\]|\]$/gm, '')); // remove trailing edit links and brackets
+
+	// Update image links to point to a corrected uri, and remove width/height attributes from images.
+	$container.find('a.image').each(function() {
+		var $a = $(this), $img = $a.find('img');
+		var href = $a.attr('href');
+		var src = $img.attr('src');
+		if (href && href.indexOf('http') !== 0) $a.attr('href', 'https://code.mcz.harvard.edu' + href);
+		$a.attr('target', '_blank');
+		if (src && src.indexOf('http') !== 0) $img.attr('src', 'https://code.mcz.harvard.edu' + src);
+		var srcset = $img.attr('srcset');
+		if (srcset) $img.attr('srcset', srcset.replace(/(\/wiki\/images\/[^\s]*)/g, "https://code.mcz.harvard.edu$1"));
+		$img.removeAttr('width').removeAttr('height');
+	});
+	$container.find('img').removeAttr('width').removeAttr('height');
+
+   $container.find('a').contents().unwrap(); // remove all <a> tags around text, leaving just the text.
+
+   // alternately, correct links to point to the wiki, uncomment the following lines to do so:
+	// TODO: Put this into the backing method, only allowed for coldfusion_user roles
+   //$container.find('a').each(function() {
+	//	var $a = $(this);
+	//	if ($a.attr('href') && $a.attr('href').indexOf('http') !== 0) {
+	//		$a.attr('href', 'https://code.mcz.harvard.edu' + $a.attr('href'));
+	//	}
+	//	$a.attr('target', '_blank');
+	//});
+}
+
+/**** End wiki content loading and processing functions ****/
+
 /** Make some readable content for a message dialog from an error message,
  * message may be empty, in which case placeholder text is returned, message
  * may start with the coldfusion responseText for a server error of <!-- \" --->,
