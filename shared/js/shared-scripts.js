c83b6502df6d119f4830da233fe400d900abcab1
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2025-09-03 13:51:07 -0400

    Redmine: 985 : Use MediaWiki API to incorporate media text into MCZbase.

--- shared/js/shared-scripts.js
+++ shared/js/shared-scripts.js
@@ -58,17 +58,17 @@ function showWiki(page, showImages, targetDiv, titleTargetDiv, openFunction, clo
 
 // When the wiki drawer is opened/closed
 function openWikiDrawer() {
-	$('##wikiDrawer').addClass('open').show();
-	$('##content').addClass('pushed');
-	$('##show-wiki').hide();
-	$('##hide-wiki').show();
+	$('#wikiDrawer').addClass('open').show();
+	$('#content').addClass('pushed');
+	$('#show-wiki').hide();
+	$('#hide-wiki').show();
 	setTimeout(updateDialogPositionForDrawer, 400);
 }
 function closeWikiDrawer() {
-	$('##wikiDrawer').removeClass('open').hide();
-	$('##content').removeClass('pushed');
-	$('##show-wiki').show();
-	$('##hide-wiki').hide();
+	$('#wikiDrawer').removeClass('open').hide();
+	$('#content').removeClass('pushed');
+	$('#show-wiki').show();
+	$('#hide-wiki').hide();
 	setTimeout(updateDialogPositionForDrawer, 0);
 }
 // Shared process/cleanup wiki content
