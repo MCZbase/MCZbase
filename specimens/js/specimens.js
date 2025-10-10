f34e38a4eec8ab5c9001f62b5ae094a4e74258b5
chicoreus <mole@morris.net>
2025-10-10 15:24:49 -0400

    ISSUE: PURPOSE: Easier view of container placement history. DESCRIPTION: Adding javascript and link to load container history into a dialog.

--- specimens/js/specimens.js
+++ specimens/js/specimens.js
@@ -317,3 +317,90 @@ function openPartContainersDialog(collection_object_id, dialogid) {
 		}
 	});
 }
+
+/** Create a dialog for displaying container history of a specimen part. 
+  * 
+  * @param container_id the primary key for the container for which to retrieve the container 
+  *   placement history.
+  * @param dialogid the id of the div that is to contain the dialog, without a leading # selector.
+  */
+function openPartContainerHistoryDialog(container_id, dialogid) { 
+	var title = "Part Container Placement";
+	console.log(dialogid);
+	var dialogElement = $("#" + dialogid);
+
+	// Check if dialog already exists and close it first if it does
+	if (dialogElement.hasClass('ui-dialog-content')) {
+		try {
+			dialogElement.dialog('close');
+			dialogElement.dialog('destroy');
+		} catch (e) {}
+	}
+
+	var content = '<div id="'+dialogid+'_div" class="col-12 px-1 px-xl-2">Loading....</div>';
+	var thedialog = dialogElement.html(content)
+	.dialog({
+		title: title,
+		autoOpen: false,
+		dialogClass: 'ui-widget-header left-3',
+		modal: false,
+		stack: true,
+		height: 'auto',
+		width: 'auto',
+		maxWidth: 600,
+		minHeight: 500,
+		draggable:true,
+		buttons: {
+			"Close Dialog": function() {
+				$(this).dialog('close');
+			}
+		},
+		open: function (event, ui) {
+			// center the dialog in the viewport
+			var dialog = $(this).dialog('widget');
+			dialog.css({
+				'position': 'fixed',
+				'top': '50%',
+				'left': '50%',
+				'transform': 'translate(-50%, -50%)'
+			});
+
+			if (typeof(getMaxZIndex) === "function") { 
+				// force the dialog to lay above any other elements in the page.
+				var maxZindex = getMaxZIndex();
+				$('.ui-dialog').css({'z-index': maxZindex + 6 });
+				$('.ui-widget-overlay').css({'z-index': maxZindex + 5 });
+			}; 
+		},
+		close: function(event,ui) {
+			setTimeout(function() { 
+				$("#"+dialogid+"_div").remove();
+			}, 100);
+		}
+	});
+	thedialog.dialog('open');
+	jQuery.ajax({
+		url: "/containers/component/public.cfc",
+		type: "get",
+		data: {
+			method: "getContainerHistoryHTML",
+			returnformat: "plain",
+			container_id: container_id
+		},
+		success: function(data) {
+			console.log("data loaded");
+			console.log($("#"+dialogid+"_div").length);
+			console.log($("#"+dialogid).length);
+			$("#"+dialogid+"_div").html(data);
+		},
+		error: function (jqXHR, status, error) {
+			var message = "";
+			if (error == 'timeout') { 
+				message = ' Server took too long to respond.';
+			} else { 
+				message = jqXHR.responseText;
+			}
+			$("#"+dialogid+"_div").html("Error (" + error + "): " + message );
+		}
+	});
+}
