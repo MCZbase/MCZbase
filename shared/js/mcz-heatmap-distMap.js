bbffb8c8d40f9c03b160844a87b9e4886bd3e252
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2026-03-02 17:39:52 -0500

    Redmine Issue 1013. Google Maps -- removing heat layer, providing deckgl as replacement - Named Group page now; Done with agent page

--- shared/js/mcz-heatmap-distMap.js
+++ shared/js/mcz-heatmap-distMap.js
@@ -179,13 +179,11 @@ function changeGradient() {
     ];
 	
     var altGradient = [
-      [193, 120, 250, 255], // purple
-      [250,  22, 160,  98], // pink
-      //[224,  22, 250,  98],  // purple
-      [250, 162,  22,  98], // light orange
-      [250, 125,  22,  158], // orange
-     // [250,  25,   0,  98], //bright orange 
-      [255,  237, 27, 255] // yellow
+      [229, 255, 25, 255], // light green
+      [254, 234, 25, 255], // yellow
+      [254, 189,  3,  255],  // orange
+      [252, 127,  1, 255], // dark orange
+      [255, 159,  2,  255] // orange
     ];
 	
   // Check what we’re currently using
