e96621668979e57f1f6270932e3528424c8b0fa9
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2026-03-06 17:10:50 -0500

    Redmine Issue 985 PURPOSE Proof of concept for MediaWiki API launched from each MCZbase form. Switching the js order at the bottom of the page.

--- shared/js/wikiDrawer.js
+++ shared/js/wikiDrawer.js
@@ -12,7 +12,7 @@
  * @param titleLink boolean indicating whether to create a link to the wiki page in the title div, true to create a link, false to just show the title.
  * @param section optional, the section number to load from the wiki page, default 0 for the entire wiki article.
  */
-function showWiki(page, showImages, targetDiv, titleTargetDiv, openFunction, closeFunction, titleLink, section=null) {
+function showWiki(page, showImages, targetDiv, titleDivId, openFunction, closeFunction, canEdit, section, onSuccess) {
 	$('#'+targetDiv).html('Loading...');
 	if (titleLink) {
 		$('#'+titleTargetDiv).html('Wiki Article: <a href="https://code.mcz.harvard.edu/wiki/index.php?title=' + page + '" target="_blank">' + page + '</a>');
