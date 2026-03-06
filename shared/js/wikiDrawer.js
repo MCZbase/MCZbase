e19adf6cfd07e7b40afd5705f0dcabc665fc65e3
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2026-03-06 18:17:37 -0500

    Redmine Issue 985 PURPOSE Proof of concept for MediaWiki API launched from each MCZbase form. Reworking parts of the ajax call.

--- shared/js/wikiDrawer.js
+++ shared/js/wikiDrawer.js
@@ -80,7 +80,7 @@ function initWikiDrawer(options) {
                 'wiki-content-title',
                 openWikiDrawer,
                 closeWikiDrawer,
-                titleLink,
+                true,
                 0
             );
 
