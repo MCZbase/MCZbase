a2dfc84dbafb6772d046a39a57a6c11eafa03b59
chicoreus <mole@morris.net>
2026-07-09 12:26:43 -0400

    ISSUE: Redmine 817 PURPOSE: Cleanup container redesigned code DESCRIPTION: Conforming details buttons to conform with style guide expectations for alternative view buttons, distinguishing it from View button using btn-outline-info.

--- containers/js/containers.js
+++ containers/js/containers.js
@@ -432,7 +432,7 @@ function buildContainerActionClass(baseClass, spacingClass) {
  */
 function buildContainerDetailsButton(containerId, displayName, feedbackId, spacingClass) {
 	return $('<button type="button"></button>')
-		.addClass(buildContainerActionClass('btn btn-xs btn-info', spacingClass || TREE_ACTION_SPACING_CLASS))
+		.addClass(buildContainerActionClass('btn btn-xs btn-outline-info', spacingClass || TREE_ACTION_SPACING_CLASS))
 		.text('Details')
 		.on('click', function() {
 			openContainerDetailsDialog(containerId, displayName, feedbackId, false);
@@ -1439,7 +1439,7 @@ function renderPositionsGrid(positions, numPositions, targetDivId, feedbackId) {
 			var occupantDisplay = position.content_container_id
 				? formatContainerDisplay(position.content_barcode, position.content_label)
 				: 'Empty';
-			var actionBtn = $('<button class="btn btn-xs btn-info" type="button"></button>')
+			var actionBtn = $('<button class="btn btn-xs btn-outline-info" type="button"></button>')
 				.text('Details')
 				.on('click', function() {
 					openContainerDetailsDialog(detailContainerId, occupantDisplay, feedbackId, false);
@@ -1862,7 +1862,7 @@ function renderTreeNodes(nodes, targetDivId, feedbackId, appendToExisting, paren
 				var inlineLeafDiv = $('<div class="tree-node-inline-leaf"></div>');
 				inlineLeafDiv.append($('<span class="tree-node-leaf-info small text-muted"></span>').text('⤷ ' + childDisplay));
 				inlineLeafDiv.append(
-					$('<button class="btn btn-info btn-xs p-0 ml-1" type="button"></button>')
+					$('<button class="btn btn-outline-info btn-xs p-0 ml-1" type="button"></button>')
 						.text('Details')
 						.on('click', function() {
 							openContainerDetailsDialog(cid, displayName, feedbackId, false);
