de0c2dda7c53f1545ca08c08313de2c5c67730a0
chicoreus <mole@morris.net>
2026-08-20 14:17:10 -0400

    ISSUE: Redmine 817 PURPOSE: Cleanup DESCRIPTION: Adding missing copyright/file description header to new js file.

--- containers/js/placePartInContainer.js
+++ containers/js/placePartInContainer.js
@@ -1,3 +1,22 @@
+/** containers/js/placePartInContainer.js
+
+Scripts supporting the placePartInContainer.cfm page for putting parts 
+from a cataloged item into containers.
+
+Copyright 2026 President and Fellows of Harvard College
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 /**
  * Backing JS for containers/placePartInContainer.cfm -- loads the specimen-search results (a
  * table of every part found, each with a checkbox to include it in the move) and the New Part
